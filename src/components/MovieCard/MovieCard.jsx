263ec3c0363c3b8ed5663c507e5cb7fcb002c327
Sakura Tanaka <sito6496t@google.com>
2023-10-16 20:32:24 +0200

    Homepage looks like the same as Technigo's and added reset.css

---
+++ src/components/MovieCard/MovieCard.jsx
@@ -0,0 +1,27 @@
+import { useState } from "react";
+import styles from "./MovieCard.module.css";
+function MovieCard({ movie }) {
+  const [onMouse, setOnMouse] = useState(false);
+  const { title, release_date, poster_path: poster } = movie;
+  console.log(movie);
+  return (
+    <div
+      className={styles.card_wrapper}
+      onMouseEnter={() => setOnMouse(true)}
+      onMouseLeave={() => setOnMouse(false)}
+    >
+      <img
+        src={`https://image.tmdb.org/t/p/w780${poster}`}
+        style={{ filter: onMouse ? "brightness(20%)" : "" }}
+      />
+      <div className={styles.inner_card}>
+        <div className={styles.text_box} style={{ display: onMouse ? "block" : "none" }}>
+          <h3>{title}</h3>
+          <p>Released : {release_date}</p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default MovieCard;
