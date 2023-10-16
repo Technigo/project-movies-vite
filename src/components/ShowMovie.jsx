d8d34a0a08eca962abcc8343ea3402ddce4706a1
Isa Robertini <robertiniisa@gmail.com>
2023-10-16 14:10:26 +0200

    test

--- src/components/ShowMovie.jsx
+++ src/components/ShowMovie.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 //1.use params to get id 2. fetch the data 3.
 const ShowMovie = () => {
     return (
-        <div>ShowMovie</div>
+        <div>ShowMovie!!!</div>
     )
 }
 
