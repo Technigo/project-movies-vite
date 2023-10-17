fa8770fe9d024459d2cd78db28d759fbee9395bc
Isa Robertini <robertiniisa@gmail.com>
2023-10-17 12:10:09 +0200

    added showmovie component and css

---
+++ src/components/MovieList.jsx
@@ -0,0 +1,48 @@
+import { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+import "../styling/movielist.css";
+
+const MovieList = () => {
+    const [movieList, setMovieList] = useState([]);
+    const [listChoice, setListChoice] = useState("movie/upcoming");
+
+    useEffect(() => {
+        fetch(
+            `https://api.themoviedb.org/3/${listChoice}?api_key=86aa5020ba0683214eaaed8f53bcbb6f`
+        )
+            .then((response) => response.json())
+            .then((data) => {
+                const results = data.results;
+                console.log(results);
+                setMovieList(results);
+            })
+            .catch((error) => console.error("Error fetching: ", error));
+    }, [listChoice]);
+
+    return (
+        <div className="home-page-container">
+            <section className="home-page">
+                {movieList.map((movie, index) => {
+                    return (
+                        <Link key={index} to={`/movies/${movie.id}`}>
+                            <div className="movie-card">
+                                <img
+                                    src={`http://image.tmdb.org/t/p/w342/${movie.poster_path}`}
+                                    alt="movie image"
+                                    className="movie-poster"
+                                />
+
+                                <h1>{movie.title}</h1>
+                                <div className="description">
+                                    <p>Release {movie.release_date}</p>
+                                </div>
+                            </div>
+                        </Link>
+                    );
+                })}
+            </section>
+        </div>
+    );
+};
+
+export default MovieList;
--- src/components/ShowMovie.jsx
+++ src/components/ShowMovie.jsx
@@ -1,10 +1,68 @@
-import React from 'react'
+import { useState, useEffect } from "react";
+import { useParams, useNavigate } from "react-router-dom";
+import "../styling/showmovie.css"
 
-//1.use params to get id 2. fetch the data 3.
-const ShowMovie = () => {
+export const ShowMovie = () => {
+    const { id } = useParams();
+    console.log(id)
+    const [movie, setMovie] = useState([]);
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        const apiKey = "86aa5020ba0683214eaaed8f53bcbb6f";
+
+        fetch(`https://api.themoviedb.org/3/movie/${id}?api_key=${apiKey}&language=en-US`)
+            .then(response => response.json())
+            .then(data => {
+                console.log(data); // Log the data to the console
+                setMovie(data);
+            })
+            .catch(error => console.error('Error fetching data:', error));
+    }, [id]);
+
+    const backButton = () => {
+        navigate("/");
+    };
+
     return (
-        <div>ShowMovie!!!</div>
-    )
-}
 
-export default ShowMovie
+        <>
+
+            <div className="MovieContainer"
+                style={{
+                    backgroundImage: `linear-gradient(rgba(0, 0, 0, 0) 10%, rgb(0, 0, 0) 100%),url(http://image.tmdb.org/t/p/w1280/${movie.backdrop_path})`,
+                }}
+            >
+
+                <button className="back-button" type="button" onClick={backButton}>
+                    <img width="32" height="32" src="https://cdn.glitch.global/b7fc725b-f734-44d3-a7ec-11457f9b7762/icons8-backwards-32.png?v=1697536318888" alt="external-backwards-arrows-kmg-design-glyph-kmg-design-1" />
+                    <span className="back-text">Back</span>
+                </button>
+
+                <div className="textContainer">
+                    <img
+                        className="MoviePoster"
+                        src={`http://image.tmdb.org/t/p/w342/${movie.poster_path}`}
+                        alt={`Poster of ${movie.title}`}
+                    />
+                    <div className="infoContainer">
+                        <div className="titleRatingContainer">
+                            <h1>{movie.title}</h1>
+                            <p className="average">
+                                ⭐️{Math.round(movie.vote_average * 10) / 10}
+                            </p>
+                        </div>
+                        <div className="movieDescription">
+                            <p>{movie.overview}</p>
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+
+        </>
+
+
+    );
+
+}
