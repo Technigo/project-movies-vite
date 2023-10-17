c59192b535128da32f80c7bfad2bcc54b4d54876
MikoZhu <huadanzhu01@gmail.com>
2023-10-17 10:15:44 +0200

    add responsive design and overplay design

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -5,7 +5,7 @@ import "../styling/movielist.css";
 const MovieList = () => {
   const [movieList, setMovieList] = useState([]);
   const [listChoice, setListChoice] = useState("movie/upcoming");
-
+  // fetch the upcoming movie list
   useEffect(() => {
     fetch(
       `https://api.themoviedb.org/3/${listChoice}?api_key=86aa5020ba0683214eaaed8f53bcbb6f`
@@ -21,26 +21,27 @@ const MovieList = () => {
 
   return (
     <div className="home-page-container">
-      <section className="home-page">
-        {movieList.map((movie, index) => {
-          return (
-            <Link key={index} to={`/movies/${movie.id}`}>
-              <div className="movie-card">
+      {movieList.map((movie, index) => {
+        return (
+          <Link key={index} to={`/movies/${movie.id}`}>
+            <div className="movie-card">
+              <div className="image-container">
                 <img
                   src={`http://image.tmdb.org/t/p/w342/${movie.poster_path}`}
                   alt="movie image"
                   className="movie-poster"
                 />
-
-                <h1>{movie.title}</h1>
-                <div className="description">
-                  <p>Release {movie.release_date}</p>
+                <div className="overplay">
+                  <h1>{movie.title}</h1>
+                  <div className="description">
+                    <p>Release {movie.release_date}</p>
+                  </div>
                 </div>
               </div>
-            </Link>
-          );
-        })}
-      </section>
+            </div>
+          </Link>
+        );
+      })}
     </div>
   );
 };
