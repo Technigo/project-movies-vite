fab4a825aadcd743358fbb2457bea20e32f9ab22
EvelynDelCarmen <hey@evelyndelcarmen.com>
2023-10-17 10:53:51 +0200

    app.jsx + components

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,29 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { MovieList } from "./Components/MovieList";
+import { SingleMovie } from "./Components/SingleMovie";
+
+
+
 export const App = () => {
-  return <div>Find me in src/app.jsx!</div>;
-};
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<MovieList />} />
+        <Route path="/:" element={<SingleMovie />} />
+      </Routes>
+
+    </BrowserRouter>
+  )
+}
+
+
+//2 olika fetch movielist + singlemovie
+
+
+
+// export const App = () => {
+//   return <div>Find me in src/app.jsx!</div>;
+// };
+
+
+// API KEY= 833874c10582aaa2144a7cb70148fbd3
