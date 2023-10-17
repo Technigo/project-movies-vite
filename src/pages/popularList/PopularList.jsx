9e46deb8c2256bd9c1012ea4a83f8c38a2e7914d
LauraLyckholm <laura.lyckholm@gmail.com>
2023-10-17 20:39:25 +0200

    Fetched popular movie information and images, showing them in moviecard. Added some styling to moviecard and some other small places to make it look better. There is a strange line between the cards that I haven't yet removed

--- src/pages/popularList/PopularList.jsx
+++ src/pages/popularList/PopularList.jsx
@@ -1,36 +1,45 @@
-// POPULAR MOVIES: https://api.themoviedb.org/3/movie/popular?api_key={api_key}&language=en-US&page=1
-// IMAGE URL FINDER: https://api.themoviedb.org/3/configuration?api_key={api_key}  FOR EXAMPLE: https://image.tmdb.org/t/p/w1280/5myQbDzw3l8K9yofUXRJ4UTVgam.jpg
-
-// import.meta.env-approach for storing api-key, so that it won't get exposed to everyone. At the same time added "netlify.toml" for accurate redirects - neccessary when using React Router.
-
-const API_KEY = import.meta.env.VITE_MOVIEDB_KEY;
-// const API_KEY = "8310ae58e125eac683c38e74d6f6a3aa";
-const URL_START = "https://api.themoviedb.org/3/movie/";
-console.log(API_KEY);
-
-const popularMovies = `${URL_START}popular?api_key=${API_KEY}&language=en-US&page=1`;
-console.log(popularMovies);
-
-// TODO: This route is responsible for the home page. It uses useEffect to run an API request to themoviedb.org and fetch popular films in the US, puts them into state using useState, and then renders them wrapped in a Link from react-router-dom to link to the detail page.
-
+import { useEffect, useState } from 'react';
 import { MovieCard } from '../../components/movieCard/MovieCard';
+import "./popularList.css";
+
+// Variables global for PopularList.jsx
+const API_KEY = import.meta.env.VITE_MOVIEDB_KEY; // import.meta.env-approach for storing api-key, so that it won't get exposed to anyone. At the same time added "netlify.toml" for accurate redirects - neccessary when using React Router.
+const URL_START = "https://api.themoviedb.org/3/movie/";
+const popularMovies = `${URL_START}popular?api_key=${API_KEY}&language=en-US&page=1`;
 
 export const PopularList = () => {
-    return (
-        <section>
-            {/* <div>
-        <ul>
-          {songs.map((song) => (
-            <li key={song.rank}>
-              <Link to={`/song/${song.title.toLowerCase().replace(/ /g, "-")}`}>
-                {song.title}
-              </Link>
-            </li>
-          ))}
-        </ul>
-      </div> */}
+    const [movies, setMovies] = useState(null);
 
-            <MovieCard />
+    // Function to fetch movies from the MovieDB-API
+    const fetchData = async () => {
+        try {
+            const response = await fetch(popularMovies);
+
+            if (!response.ok) {
+                throw new Error("Response was not ok") // Handle error, show an error-message if something goes wrong.
+            }
+
+            const data = await response.json();
+            setMovies(data);
+        } catch (error) {
+            console.error("Something went wrong!") // Log the error to the console
+        }
+    }
+
+    // Handles the fetch from above
+    useEffect(() => {
+        fetchData()
+    }, [])
+
+
+    return (
+        // If there is movie-data, then send the results to MovieCard.jsx, otherwise show a loading message. Here we could add a better looking loader.
+        <section className="popular-movies">
+            {movies ? (
+                <MovieCard movieData={movies.results} />
+            ) : (
+                <p>Loading...</p>
+            )}
         </section>
     )
 }
