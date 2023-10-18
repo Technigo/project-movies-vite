43afc9d8325a1f318c66aa8678c53e3261a6ef65
sheryll.poo-cheong <shey_pc@gmail.com>
2023-10-18 14:56:38 +0200

    created MovieDetails.jsx + affischImage

--- src/components/MovieDetails.jsx
+++ src/components/MovieDetails.jsx
@@ -1,7 +1,15 @@
-import React from 'react'
+export const MovieDetails = ({ movie }) => {
+    console.log(movie);
 
-export const MovieDetails = () => {
+    const { title, backdrop_path, overview } = movie;
+    const affischImage = `https://image.tmdb.org/t/p/w342/${backdrop_path}`;
+
     return (
-        <div>MovieDetails</div>
+        <div>
+            <img src={affischImage} alt={overview} />
+
+            <h1>{title}</h1>
+            <p>{overview}</p>
+        </div>
     )
 }
