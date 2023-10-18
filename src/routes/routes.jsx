499ca1090d0e30e664c4094a49e34769e9660b1c
sheryll.poo-cheong <shey_pc@gmail.com>
2023-10-18 14:48:32 +0200

    removing PageNotFound for netlify deploy/temporary

--- src/routes/routes.jsx
+++ src/routes/routes.jsx
@@ -1,13 +1,13 @@
 import { Route } from "react-router-dom";
 import { MovieList } from "../pages/MovieList";
 import { ShowMeMovie } from "../pages/ShowMeMovie";
-import { PageNotFound } from "../pages/PageNotFound";
+// import { PageNotFound } from "../pages/PageNotFound";
 
 export const routes = (
     <>
-        <Route path="*" element={<PageNotFound />} />
+        {/* <Route path="*" element={<PageNotFound />} /> */}
         <Route path="/" element={<MovieList />} />
-        <Route path="/movie:id" element={<ShowMeMovie />} />
+        <Route path="/movie/:id" element={<ShowMeMovie />} />
 
     </>
 );
