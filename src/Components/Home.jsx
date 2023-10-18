7d41320fd29bc1559e8d5e6e39117c685f08f0e0
Malin <malinlundhe@hotmail.com>
2023-10-18 19:47:23 +0200

    started with homepage fetching data

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,7 +1,26 @@
-
+import React, { useState, useEffect } from 'react';
 
 export const Home = () => {
+  const [data, setData] = useState(null);
+  const apiKey = '0c1859339f764c871c4c8225088979ea';
+
+  useEffect(() => {
+  fetch(`https://api.themoviedb.org/3/movie/popular?api_key=${apiKey}&language=en-US&page=1`)
+  .then(res => res.json())
+  .then(data => setData(data))
+  .catch(error => alert("Error fetching data:", error));
+  }, [])
+
+  if (!data) return <h4>Loading...</h4>
+  
+  
   return (
-    <div>Home</div>
-  )
-}
+    <div>
+    <ul>
+      {data.results.map(movie => (
+        <li key={movie.id}>{movie.title}</li>
+      ))}
+    </ul>
+    </div>
+  );
+};
