7513020d229f1ddd842169c754417e9d0c9a62d9
veronica astrom <v.aastrom@gmail.com>
2023-10-18 14:18:17 +0200

    removed a comment

--- src/routes/routes.jsx
+++ src/routes/routes.jsx
@@ -10,7 +10,6 @@ export const routes = (
     <>
         <Route path="/" element={<MovieList />} />
         <Route path="/movies/:id" element={<SingleMovie />} />
-        {/* <Route path="*" element={<NotFoundPage />} /> */}
     </>
 );
 
