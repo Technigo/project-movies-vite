4ef6719a896dfa3d5b49cbaf043255e437dfb4a3
Sakura Tanaka <sito6496t@gmail.com>
2023-10-19 13:59:41 +0200

    suspense tried

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,9 @@
+// import { Suspense, lazy } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+// import { LoadingFullPage } from "./components/LoaingFullPage";
+
+// const HomePage = lazy(() => import("./pages/HomePage"));
+// const Detail = lazy(() => import("./pages/Detail"));
 import { HomePage } from "./pages/HomePage";
 import { Detail } from "./pages/Detail";
 
@@ -7,7 +12,9 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<HomePage />} />
+        {/* <Suspense fallback={<LoadingFullPage />}> */}
         <Route path="/movie/:id" element={<Detail />} />
+        {/* </Suspense> */}
       </Routes>
     </BrowserRouter>
   );
