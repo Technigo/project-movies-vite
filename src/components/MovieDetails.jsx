d009cfe09f55edad2cee1c34c033c7fee1ed4aa8
Klaudia Wróblewska <kwroblewska0410@gmail.com>
2023-10-19 11:32:37 +0200

    fix the style

--- src/components/MovieDetails.jsx
+++ src/components/MovieDetails.jsx
@@ -24,16 +24,3 @@ export const MovieDetails = ({ movie }) => {
     </div>
   );
 };
-
-// export const MovieDetails = ({ movie }) => {
-//     const { title, backdrop_path, overview } = movie;
-//     const backgroundImage = `https://image.tmdb.org/t/p/w342/${backdrop_path}`;
-
-//     return (
-//       <div>
-//         <img src={backgroundImage} alt={overview} />
-//         <h1>{title}</h1>
-//         <p>{overview}</p>
-//       </div>
-//     );
-//   }
