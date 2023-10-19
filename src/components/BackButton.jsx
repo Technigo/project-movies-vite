db610103747bd913dd7cb1fa024572b227d28c21
mariateresepettersson <mariateresepettersson>
2023-10-19 17:40:48 +0100

    Styling added to button

--- src/components/BackButton.jsx
+++ src/components/BackButton.jsx
@@ -1,11 +1,15 @@
 // This component represents a "Go Back" button to navigate back to the list of movies. It's a reusable component.
 import "./backbutton.css";
 import { Link } from 'react-router-dom';
+import arrowIcon from './arrow.svg'; 
 
 export function BackButton() {
   return (
-    <Link to="/">
-      <button>Go Back</button>
-    </Link>
+    <div className="button-container">
+      <Link to="/" className="button">
+        <img src={arrowIcon} alt="Arrow" className="icon" /> 
+        Go Back
+      </Link>
+    </div>
   );
 }
