933e6d6ef63fe0a7ab7810a34e75e4f110c475b4
Andrej <72137459+andrkavr@users.noreply.github.com>
2023-10-19 09:54:56 +0200

    some styling added to when movie is clicked

--- src/components/movie-card/MoviePreview.jsx
+++ src/components/movie-card/MoviePreview.jsx
@@ -1,13 +1,23 @@
 export const MoviePreview = ({ movie }) => {
   console.log(movie);
-  const { title, backdrop_path, overview } = movie;
-  const backgroundImage = `https://image.tmdb.org/t/p/w342/${backdrop_path}`;
-
+  const { title, backdrop_path, overview, poster_path, vote_average } = movie;
+  const backgroundImage = `https://image.tmdb.org/t/p/w1280/${backdrop_path}`;
+  const posterImage = `https://image.tmdb.org/t/p/w342/${poster_path}`;
   return (
-    <div>
-      <img src={backgroundImage} alt={overview} />
-      <h1>{title}</h1>
-      <p>{overview}</p>
+    <div
+      className="movie-container"
+      style={{ backgroundImage: `url(${backgroundImage}) center center` }}
+    >
+      <div className="movie-summary">
+        <img src={posterImage} alt={title} />
+        <div className="movie-info">
+          <div className="title-and-score">
+            <h1 className="movie-title">{title}</h1>
+            <h2 className="movie-score"> ⭐{vote_average}</h2>
+          </div>
+          <p className="movie-overview">{overview}</p>
+        </div>
+      </div>
     </div>
   );
 };
