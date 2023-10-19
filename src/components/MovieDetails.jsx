b5550d787f4b3b1fbb57f5656c5f73bdc3e2655e
sheryll.poo-cheong <shey_pc@gmail.com>
2023-10-19 10:00:29 +0200

    modifying code for styling

--- src/components/MovieDetails.jsx
+++ src/components/MovieDetails.jsx
@@ -1,15 +1,30 @@
+import '../styling/moviedetails.css'
 export const MovieDetails = ({ movie }) => {
     console.log(movie);
 
-    const { title, backdrop_path, overview } = movie;
-    const affischImage = `https://image.tmdb.org/t/p/w342/${backdrop_path}`;
+    const { title, poster_path, backdrop_path, overview, vote_average } = movie;
+    const affischImage = `https://image.tmdb.org/t/p/w342/${poster_path}`;
+    const backgroundImage = `https://image.tmdb.org/t/p/w1280/${backdrop_path}`
 
     return (
-        <div>
-            <img src={affischImage} alt={overview} />
-
-            <h1>{title}</h1>
-            <p>{overview}</p>
-        </div>
+        <>
+            <div className='single-movie-background'
+                style={{
+                    backgroundImage: `url(${backgroundImage})`,
+                    backgroundRepeat: "no-repeat",
+                    backgroundSize: "cover",
+                    backgroundAttachment: "fixed"
+                }}>
+                <div className="single-movie-card">
+                    <img src={affischImage} alt={overview} />
+                    <div className="single-movie-info">
+                        <h2 >{title}
+                            <span > ⭐{vote_average.toFixed(1)} </span>
+                        </h2>
+                        <p>{overview}</p>
+                    </div>
+                </div>
+            </div >
+        </>
     )
 }
