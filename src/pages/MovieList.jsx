c24a6ae2d431e697370c242891b057bcecf32f22
sheryll.poo-cheong <shey_pc@gmail.com>
2023-10-20 21:06:25 +0200

    centering the loader in MovieList.jsx

--- src/pages/MovieList.jsx
+++ src/pages/MovieList.jsx
@@ -1,47 +1,10 @@
-// import { useState, useEffect } from "react"
-// import { Link } from "react-router-dom"
-// import { Oval } from "react-loader-spinner"
-// export const MovieList = () => {
-//     const [movieList, setMovieList] = useState([])
-
-//     const API_KEY = 
-
-
-//     useEffect(() => {
-
-//         fetch(`https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}&language=en-US&page=`)
-//             .then(response => response.json())
-//             .then(data => {
-//                 const results = data.results
-//                 console.log(results)
-//                 setMovieList(results)
-//             })
-//             .catch(err => console.error(err));
-//     }, [])
-
-//     return (
-
-
-//                 {movieList.map((movie, index) => {
-//                     return (
-//                         <Link to={`/movie/${movie.id}`} key={index}>
-//                             <img src={`http://image.tmdb.org/t/p/w342/${movie.poster_path}`} alt="" />
-//                             <p>{movie.id}</p>
-//                         </Link>
-
-//                     )
-//                 })}
-//             </div>
-//         </div>
-//     )
-// }
-
-
 import { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import { Oval } from "react-loader-spinner"
 import '../styling/movieList.css';
+
 export const MovieList = () => {
+
     const [movieList, setMovieList] = useState([]);
     const [loading, setLoading] = useState(false)
     const apiEnv = import.meta.env.VITE_OPENDB_KEY;
@@ -56,20 +19,25 @@ export const MovieList = () => {
             })
             .catch(err => console.error(err));
     }, [apiEnv]);
+
     return (
         <div>
             <div>
                 {loading && (<Oval
-                    height={80}
-                    width={80}
+                    height={100}
+                    width={100}
                     color="#4fa94d"
-                    wrapperStyle={{}}
+                    wrapperStyle={{
+                        display: "flex",
+                        justifyContent: "center",
+                        marginTop: "10%"
+                    }}
                     wrapperClass=""
                     visible={true}
                     ariaLabel='oval-loading'
                     secondaryColor="#4fa94d"
-                    strokeWidth={2}
-                    strokeWidthSecondary={2}
+                    strokeWidth={5}
+                    strokeWidthSecondary={3}
                 />
                 )
                 }
