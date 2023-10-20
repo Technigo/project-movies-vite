2cb2c4d3ad2c90e99a900bc84493223b9141db99
MikoZhu <huadanzhu01@gmail.com>
2023-10-20 12:56:43 +0200

    delete some commands

--- src/App.jsx
+++ src/App.jsx
@@ -7,16 +7,6 @@ import PageNotFound from "./components/PageNotFound";
 export const App = () => {
   return (
     <BrowserRouter>
-      {/* <nav>
-        <ul>
-          <li>
-            <Link to="/"> Home</Link>
-          </li>
-          <li>
-            <Link to="/Movie"> Movie</Link>
-          </li>
-        </ul>
-      </nav> */}
       <Routes>
         <Route path="/" element={<PopularList />} />
         <Route path="/movies/:id" element={<SingleMovie />} />
