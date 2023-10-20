d5a69b5e420755aa03ad0383e24f87685282d156
sheryll.poo-cheong <shey_pc@gmail.com>
2023-10-20 21:09:03 +0200

    reactivated PageNotFound+styling

--- src/routes/routes.jsx
+++ src/routes/routes.jsx
@@ -1,13 +1,12 @@
 import { Route } from "react-router-dom";
 import { MovieList } from "../pages/MovieList";
 import { ShowMeMovie } from "../pages/ShowMeMovie";
-// import { PageNotFound } from "../pages/PageNotFound";
+import { PageNotFound } from "../pages/PageNotFound";
 
 export const routes = (
     <>
-        {/* <Route path="*" element={<PageNotFound />} /> */}
+        <Route path="/*" element={<PageNotFound />} />
         <Route path="/" element={<MovieList />} />
         <Route path="/movie/:id" element={<ShowMeMovie />} />
-
     </>
 );
