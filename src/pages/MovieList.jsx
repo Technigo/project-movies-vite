49b1f253d5259ceed6740d7683bccc03a11eb040
sheryll.poo-cheong <shey_pc@gmail.com>
2023-10-20 09:28:58 +0200

    So Youn: created home page MovieList.jsx

--- src/pages/MovieList.jsx
+++ src/pages/MovieList.jsx
@@ -1,53 +1,91 @@
-import { useState, useEffect } from "react"
-import { Link } from "react-router-dom"
-// import { Oval } from "react-loader-spinner"
+// import { useState, useEffect } from "react"
+// import { Link } from "react-router-dom"
+// // import { Oval } from "react-loader-spinner"
+// export const MovieList = () => {
+//     const [movieList, setMovieList] = useState([])
+//     // const [loading, setLoading] = useState(false)
+//     const API_KEY = "d702e384b1621c01acd5bc0e04a75981"
+
+
+//     useEffect(() => {
+
+//         fetch(`https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}&language=en-US&page=`)
+//             .then(response => response.json())
+//             .then(data => {
+//                 const results = data.results
+//                 console.log(results)
+//                 setMovieList(results)
+//             })
+//             .catch(err => console.error(err));
+//     }, [])
+
+//     return (
+
+//         <div>
+//             {/* <Oval
+//                 height={80}
+//                 width={80}
+//                 color="#4fa94d"
+//                 wrapperStyle={{}}
+//                 wrapperClass=""
+//                 visible={true}
+//                 ariaLabel='oval-loading'
+//                 secondaryColor="#4fa94d"
+//                 strokeWidth={2}
+//                 strokeWidthSecondary={2}
+//             /> */}
+//             <div>
+//                 {movieList.map((movie, index) => {
+//                     return (
+//                         <Link to={`/movie/${movie.id}`} key={index}>
+//                             <img src={`http://image.tmdb.org/t/p/w342/${movie.poster_path}`} alt="" />
+//                             <p>{movie.id}</p>
+//                         </Link>
+
+//                     )
+//                 })}
+//             </div>
+//         </div>
+//     )
+// }
+
+
+import { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+import '../styling/MovieList.css';
 export const MovieList = () => {
-    const [movieList, setMovieList] = useState([])
-    // const [loading, setLoading] = useState(false)
-    const API_KEY = "d702e384b1621c01acd5bc0e04a75981"
-
-
+    const [movieList, setMovieList] = useState([]);
+    const API_KEY = "d702e384b1621c01acd5bc0e04a75981";
     useEffect(() => {
-
         fetch(`https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}&language=en-US&page=`)
             .then(response => response.json())
             .then(data => {
-                const results = data.results
-                console.log(results)
-                setMovieList(results)
+                const results = data.results;
+                setMovieList(results);
             })
             .catch(err => console.error(err));
-    }, [])
-
+    }, []);
     return (
-
         <div>
-            {/* <Oval
-                height={80}
-                width={80}
-                color="#4fa94d"
-                wrapperStyle={{}}
-                wrapperClass=""
-                visible={true}
-                ariaLabel='oval-loading'
-                secondaryColor="#4fa94d"
-                strokeWidth={2}
-                strokeWidthSecondary={2}
-            /> */}
             <div>
                 {movieList.map((movie, index) => {
                     return (
-                        <Link to={`/movie/${movie.id}`} key={index}>
-                            <img src={`http://image.tmdb.org/t/p/w342/${movie.poster_path}`} alt="" />
-                            <p>{movie.id}</p>
+                        <Link to={`/movie/${movie.id}`} key={index} className="movie-item">
+                            <div className="movie-image-container">
+                                <img
+                                    src={`http://image.tmdb.org/t/p/w342/${movie.poster_path}`}
+                                    alt=""
+                                    className="movie-image"
+                                />
+                                <div className="movie-title-overlay">
+                                    <div className="movie-title">{movie.title}</div>
+                                    <div className="movie-release-date">Release {movie.release_date}</div>
+                                </div>
+                            </div>
                         </Link>
-
-                    )
+                    );
                 })}
             </div>
         </div>
-    )
-}
-
-
-// /3/movie/popular
+    );
+};
