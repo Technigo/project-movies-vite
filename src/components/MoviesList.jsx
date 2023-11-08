041a8a7b6d6bca5b10ff1e2e0bb7dd4d5ae1b294
Fabio <cassisafabio97@gmail.com>
2023-11-08 19:53:30 +0100

    added spinner during loading page

--- src/components/MoviesList.jsx
+++ src/components/MoviesList.jsx
@@ -4,32 +4,47 @@ import '../css/MoviesList.css'
 
 const MoviesList = ({ apiKey }) => {
   const [movies, setMovies] = useState([]);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     const fetchMovies = async () => {
-      const response = await fetch(
-        `https://api.themoviedb.org/3/movie/popular?api_key=${apiKey}&language=en-US&page=1`
-      );
-      const data = await response.json();
-      setMovies(data.results);
+      try {
+        setLoading(true);
+        const response = await fetch(
+          `https://api.themoviedb.org/3/movie/popular?api_key=${apiKey}&language=en-US&page=1`
+        );
+        const data = await response.json();
+        setLoading(false);
+        setMovies(data.results);
+      } catch (error) {
+        setLoading(false);
+        // Handle errors here
+      }
     };
-
+  
     fetchMovies();
   }, [apiKey]);
+  
 
   return (
     <div className="MoviesList">
-      {movies.map((movie) => (
-        <Link key={movie.id} to={`/movies/${movie.id}`}>
-          <img
-            src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
-            alt={movie.title}
-          />
-          <div className="MovieDetails">
-            <h2>{movie.title}</h2>
-          </div>
-        </Link>
-      ))}
+      {loading ? (
+        // Display a loading message or spinner while waiting for data
+        <div className="LoadingSpinner"></div>
+      ) : (
+        // Display the list of movies when data is ready
+        movies.map((movie) => (
+          <Link key={movie.id} to={`/movies/${movie.id}`}>
+            <img
+              src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
+              alt={movie.title}
+            />
+            <div className="MovieDetails">
+              <h2>{movie.title}</h2>
+            </div>
+          </Link>
+        ))
+      )}
     </div>
   );
 };
