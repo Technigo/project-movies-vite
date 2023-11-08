bb587336f2159c909bda6f3b5f08bf2cde8bbb2e
Fabio <cassisafabio97@gmail.com>
2023-11-08 18:41:19 +0100

    initial cards, movie detail

--- src/components/MovieDetail.jsx
+++ src/components/MovieDetail.jsx
@@ -1,34 +1,60 @@
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import "../css/MovieDetail.css"
 
 const MovieDetail = ({ apiKey }) => {
   const [movie, setMovie] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
   const { id } = useParams();
 
   useEffect(() => {
     const fetchMovie = async () => {
-      const response = await fetch(
-        `https://api.themoviedb.org/3/movie/${id}?api_key=${apiKey}&language=en-US`
-      );
-      const data = await response.json();
-      setMovie(data);
+      try {
+        const response = await fetch(
+          `https://api.themoviedb.org/3/movie/${id}?api_key=${apiKey}&language=en-US`
+        );
+
+        if (!response.ok) {
+          throw new Error("Network response was not ok");
+        }
+
+        const data = await response.json();
+        setMovie(data);
+        setLoading(false);
+      } catch (error) {
+        setError(error.message);
+        setLoading(false);
+      }
     };
 
     fetchMovie();
   }, [id, apiKey]);
 
-  if (!movie) return <div>Loading...</div>;
+  if (loading) return <div>Loading...</div>;
+  if (error) return <div>Error: {error}</div>;
+
+  // Calculate the rating with one decimal point
+  const rating = movie.vote_average.toFixed(1);
 
   return (
-    <div>
+    <div className="MovieDetail">
       <h1>{movie.title}</h1>
       <img
         src={`https://image.tmdb.org/t/p/w1280${movie.backdrop_path}`}
         alt={movie.title}
       />
       <p>{movie.overview}</p>
+
+      <div>
+        <strong>Release Date:</strong> {movie.release_date}
+      </div>
+      <div>
+        <strong>Rating:</strong> {rating} &#11088; {/* Star emoji */}
+      </div>
     </div>
   );
 };
 
 export default MovieDetail;
+
