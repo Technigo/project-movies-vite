3fd00c6d560a607908f1473def2ca7c1156317be
Fabio <cassisafabio97@gmail.com>
2023-11-08 10:55:54 +0100

    fixed some CSS - home

--- src/components/MoviesList.jsx
+++ src/components/MoviesList.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
+import '../css/MoviesList.css'
 
 const MoviesList = ({ apiKey }) => {
   const [movies, setMovies] = useState([]);
@@ -17,14 +18,16 @@ const MoviesList = ({ apiKey }) => {
   }, [apiKey]);
 
   return (
-    <div>
+    <div className="MoviesList">
       {movies.map((movie) => (
         <Link key={movie.id} to={`/movies/${movie.id}`}>
           <img
             src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
             alt={movie.title}
           />
-          <h2>{movie.title}</h2>
+          <div className="MovieDetails">
+            <h2>{movie.title}</h2>
+          </div>
         </Link>
       ))}
     </div>
