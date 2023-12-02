d3134806a826f0cdc516dac9f81b27f9c0bf7f43
Shiqwan <ulrika.galnander@gmail.com>
2023-12-02 16:12:32 +0100

    responsive ok, done for now

--- src/Components/MainPage.jsx
+++ src/Components/MainPage.jsx
@@ -10,15 +10,15 @@ export const MainPage = () => {
       `https://api.themoviedb.org/3/movie/popular?api_key=${apiKey}&language=en-US&page=1`
     )
       .then((res) => res.json())
-      .then((data) => setData(data))
-      .catch((error) => alert("Error fetching data:" + error));
+      .then((data) => setData(data));
+    // .catch((error) => alert("Error fetching data:" + error)); This doesn't seem to be working, look into later
   }, []);
 
   if (!data) return <h2>Loading..</h2>;
 
   return (
     <div>
-      <h1 className="h1-main">Popular movies in the US</h1>
+      {/* <h1 className="h1-main">Movies headline blabla for the mobile version? And do hidden otherwise?</h1> */}
       {/* why did I put a ul here? It works but go back later */}
       <ul>
         {data.results.map((movie) => (
