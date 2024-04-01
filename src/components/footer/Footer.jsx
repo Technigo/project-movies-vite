8ec80fb621688b3a6d01b0ba3cba1e45e69b5f31
Sofia Johansson <153744881+sofia32057@users.noreply.github.com>
2024-04-01 13:56:37 +0200

    Basic styling

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -3,7 +3,21 @@ import "./Footer.css"
 export const Footer = () => {
   return (
     <footer>
-      <p>This website uses TMDB and the TMDB APIs but is not endorsed, certified, or otherwise approved by TMDB.</p>
+      <div className="contact">
+        <p>Made by</p>
+        <ul>
+          <li><span>Nathalie</span>
+            <a href="https://portfolio-nathalie.netlify.app/">View Nathalies portfolio</a>
+          </li>
+          <li><span>Sofia</span>
+            <a href="https://sofias-portfolio.netlify.app/">View Sofias portfolio</a>
+          </li>
+        </ul>
+      </div>
+      <p className="disclaimer">
+        Disclaimer: This website uses TMDB and the TMDB APIs but is not
+        endorsed, certified, or otherwise approved by TMDB.
+      </p>
     </footer>
   )
 }
