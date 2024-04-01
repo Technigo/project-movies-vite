9ae7e6274bf3d0c0abfac37b22224cb067d6888d
ohitsnathalie <152981339+ohitsnathalie@users.noreply.github.com>
2024-04-01 18:07:18 +0200

    change header and add favicon

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,22 +1,29 @@
 import "./Footer.css"
 
 export const Footer = () => {
-  return (
-    <footer>
-      <div className="contact">
-        <p>Made by</p>
-        <ul>
-          <li><span>Nathalie</span>
-            <a href="https://portfolio-nathalie.netlify.app/">View Nathalies portfolio</a>
-          </li>
-          <li><span>Sofia</span>
-            <a href="https://sofias-portfolio.netlify.app/">View Sofias portfolio</a>
-          </li>
-        </ul>
-      </div>
-      <p className="disclaimer">
-        Disclaimer: This website uses TMDB and the TMDB APIs but is not endorsed, certified, or otherwise approved by TMDB.
-      </p>
-    </footer>
-  )
+	return (
+		<footer>
+			<div className='contact'>
+				<p>Made by</p>
+				<ul>
+					<li>
+						<span>Nathalie</span>
+						<a href='https://portfolio-nathalie.netlify.app/'>
+							View Nathalie&apos;s portfolio
+						</a>
+					</li>
+					<li>
+						<span>Sofia</span>
+						<a href='https://sofias-portfolio.netlify.app/'>
+							View Sofia&apos;s portfolio
+						</a>
+					</li>
+				</ul>
+			</div>
+			<p className='disclaimer'>
+				Disclaimer: This website uses TMDB and the TMDB APIs but is not
+				endorsed, certified, or otherwise approved by TMDB.
+			</p>
+		</footer>
+	)
 }
