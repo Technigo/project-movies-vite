1f32dbf0fab009eec01c571fa0e1abb78c438d07
Wendy Zhao <wendy.w.zhao@outlook.com>
2024-04-02 16:08:29 +0200

    Create folder and component structure

---
+++ src/components/PopularList.jsx
@@ -0,0 +1,13 @@
+import { Link } from "react-router-dom";
+
+const PopularList = () => {
+  return (
+    <Link to="/">
+      <img src="" alt=""></img>
+      <h2>Movie title: </h2>
+      <p>Released: </p>
+    </Link>
+  );
+};
+
+export default PopularList;
---
+++ src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import PopularList from "../components/PopularList";
+
+const Home = () => {
+  return (
+    <div>
+      <h1>Wen&apos;s movie site</h1>
+      <PopularList />
+    </div>
+  );
+};
+
+export default Home;
