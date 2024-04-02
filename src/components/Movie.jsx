c8bac00d03b94177cc669cdf8255f24c7f1a5162
izzibizz <contact@izabellind.com>
2024-04-02 20:58:25 +0200

    fixed the bugs in the movielist fetch

--- src/components/Movie.jsx
+++ src/components/Movie.jsx
@@ -1,3 +1,11 @@
 //const key = "e185d0927f85272fbd0fd2526ecf0657";
-import { useState, useEffect } from "react";
-import { Link } from "react-router-dom";
+//import { useState, useEffect } from "react";
+//import { Link } from "react-router-dom";
+
+export const Movie = () => {
+    return(
+        <div>
+            <h2>Movie component</h2>
+        </div>
+    )
+}
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,38 +1,49 @@
 import { Link } from "react-router-dom";
+import { useState, useEffect } from 'react'
 
 export const MovieList = () => {
   const [movies, setMovies] = useState([]);
   const [loading, setLoading] = useState(true);
 
-  const fetchApi = () => {
+  const fetchMovies = () => {
     fetch(
       "https://api.themoviedb.org/3/movie/popular?api_key=e185d0927f85272fbd0fd2526ecf0657&language=en-US&page=1"
     )
-      .then((response) => response.json)
-      .then((movies) => setMovies(movies));
-    console
-      .log(movies)
-
-      .catch((error) => {
-        console.log(error);
+      .then ((response) => response.json())
+      .then ((movies) => {
+        setMovies(movies)
+        console.log(movies.results)
+        setLoading(false)
+      })
+      .catch ((err) => {
+        console.error("Error fetching movies:", err);
         setLoading(false);
       });
   };
   useEffect(() => {
-    fetchApi();
+    fetchMovies();
   }, []);
 
   return (
-    <div className="movies">
-      {movies.map((movie) => (
-        <ul key={movie.title}>
-          <li>
-            <Link to={`movie/:${movie.titletoLowerCase().replace(/ /g, "-")}`}>
+    <>
+    {loading && (
+      <div className="loading">
+        Loading
+      </div>
+      )}
+      {!loading && (
+        <div className="movies">
+          {movies.results.map((movie, index) => (
+           <ul key={index}>
+            <li>
+            <Link to={`movie/:${movie.title.toLowerCase().replace(/ /g, "-")}`}>
               {movie.title}
             </Link>
           </li>
         </ul>
       ))}
     </div>
+      )}
+    </>
   );
 };
