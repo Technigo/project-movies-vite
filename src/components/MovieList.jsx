82f75063b8306992f1f63c63c5a8ab379dd91ad2
Caracal23 <154092424+Caracal23@users.noreply.github.com>
2024-04-02 19:46:40 +0200

    move information to MovieList

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,5 +1,38 @@
-import { Movie } from "./Movie";
+import { Link } from "react-router-dom";
 
 export const MovieList = () => {
-  return <h1>Movie List</h1>;
+  const [movies, setMovies] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  const fetchApi = () => {
+    fetch(
+      "https://api.themoviedb.org/3/movie/popular?api_key=e185d0927f85272fbd0fd2526ecf0657&language=en-US&page=1"
+    )
+      .then((response) => response.json)
+      .then((movies) => setMovies(movies));
+    console
+      .log(movies)
+
+      .catch((error) => {
+        console.log(error);
+        setLoading(false);
+      });
+  };
+  useEffect(() => {
+    fetchApi();
+  }, []);
+
+  return (
+    <div className="movies">
+      {movies.map((movie) => (
+        <ul key={movie.title}>
+          <li>
+            <Link to={`movie/:${movie.titletoLowerCase().replace(/ /g, "-")}`}>
+              {movie.title}
+            </Link>
+          </li>
+        </ul>
+      ))}
+    </div>
+  );
 };
