07c820205ed68c483f52d766a033ae07507fe7e0
Alma Herrström <almaherrstrom@Almas-MacBook-Air-4.local>
2024-04-02 11:22:37 +0200

    set up API url

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,25 @@
 export const App = () => {
-  return <div>Find me in src/app.jsx!</div>;
-};
+  const API_KEY = "2bd2e4cabf6e3176951e947b95e23dd9"
+  const BASE_URL = `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}&language=en-US&page=1`
+
+  const fetchMovies = () => {
+    fetch(BASE_URL)
+      .then((response) => {
+        if (!response.ok) {
+          if (response.status === 404) {
+            throw new Error("Movie not found")
+          } else {
+            throw new Error("Failed fetching movies")
+          }
+        }
+        return response.json()
+      })
+      .then((json) => {
+        console.log(json)
+      })
+  }
+
+  fetchMovies()
+
+  return <div>Find me in src/app.jsx!</div>
+}
---
+++ src/data/copyapi.md
@@ -0,0 +1,20 @@
+{
+"adult": false,
+"backdrop_path": "/sR0SpCrXamlIkYMdfz83sFn5JS6.jpg",
+"genre_ids": [
+28,
+878,
+12
+],
+"id": 823464,
+"original_language": "en",
+"original_title": "Godzilla x Kong: The New Empire",
+"overview": "Following their explosive showdown, Godzilla and Kong must reunite against a colossal undiscovered threat hidden within our world, challenging their very existence – and our own.",
+"popularity": 5072.084,
+"poster_path": "/gmGK5Gw5CIGMPhOmTO0bNA9Q66c.jpg",
+"release_date": "2024-03-27",
+"title": "Godzilla x Kong: The New Empire",
+"video": false,
+"vote_average": 7.092,
+"vote_count": 228
+}
