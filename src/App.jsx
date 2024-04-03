94e73c827296e0883e4608d4019ee1ac5ab7bd96
Johanna Billingskog Nyberg <johanna@billingskognyberg.se>
2024-04-03 16:08:16 +0200

    Added BrowserRouter, Routes and Route in App.

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,18 @@
 import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Movies } from "./Movies";
 // import { MovieCard } from "./components/MovieCard";
 import { MovieDetails } from "./components/MovieDetails";
 
 const App = () => {
   return (
-    <>
-      <Movies />
-      {/* <MovieCard /> */}
-      <MovieDetails />
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Movies />} />
+        {/* <MovieCard /> */}
+        <Route path="/movie" element={<MovieDetails />} />
+      </Routes>
+    </BrowserRouter>
   );
 };
 
