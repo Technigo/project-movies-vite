1851161a47240092bf31bf624ac3d4bcabd2c57f
SofieFerrari <sofiestrahl@gmail.com>
2024-04-03 22:28:43 +0200

    worked on loading animation and tried to fix setTimeout

--- src/components/FirstPage.jsx
+++ src/components/FirstPage.jsx
@@ -1,9 +1,13 @@
 import React from "react"
 import '../styling/FirstPage.css'
 import { PopularList } from "./PopularList"
+import { DelayLottie } from "./Loader"
 
 export const Movies = () => {
   return (
+		<>
+		<DelayLottie />
 		<PopularList />
+		</>
 	)
 }
--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -1,7 +1,31 @@
 import React from 'react'
-import { useLottie } from 'lottie-react'
-import 
+import { useState, useEffect } from 'react'
+import Lottie from 'react-lottie'
+import animationData from '../lotties/animation.json'
 
 export const Loader = () => {
-	return
+	const [loading, setLoading] = useState(false)
+
+	const defaultOptions = {
+		loop: true,
+		autoplay: true,
+		animationData: animationData,
+		rendererSettings: {
+			preserveAspectRatio: 'xMidYMid slice',
+		},
+	}
+
+	return (
+		<div>
+			<Lottie options={defaultOptions} height={400} width={400} />
+		</div>
+	)
 }
+
+//nedan är försök att få animationen att visas längre.
+
+// useEffect(() => {
+// 	export const DelayLottie = setTimeout(() => {
+// 		setLoading(true)
+// 	}, 3000)
+// })
