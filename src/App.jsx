425fef08cbe4d1b5328db8eaee9e9f330362c49f
SofieFerrari <sofiestrahl@gmail.com>
2024-04-03 13:33:46 +0200

    pushing before starting to fetch API

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { useParams } from 'react-router-dom'
 import { Nav } from './components/Nav'
 import { Movies } from './components/FirstPage'
 import { Details } from './components/SecondPage'
