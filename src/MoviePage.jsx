60bef571ded2a5da1834ada9d80815819033a0ae
Johanna Catalini Smith <johannacatalini@gmail.com>
2024-04-03 20:30:08 +0200

    fetch data for movie

--- src/MoviePage.jsx
+++ src/MoviePage.jsx
@@ -1,6 +1,21 @@
 import { useParams } from "react-router-dom";
+import { useEffect, useState } from "react";
 
 export const MoviePage = () => {
   const { id } = useParams();
-  return <h1>movie page {id}</h1>;
+  const [movie, setMovie] = useState();
+
+  useEffect(() => {
+    const url = `https://api.themoviedb.org/3/movie/${id}?api_key=bd8739b499ca4e3054f829b6e1e0d8e8&language=en-US&page=1`;
+    fetch(url)
+      .then((response) => response.json())
+      .then((data) => setMovie(data));
+  }, []);
+  console.log(movie);
+
+  if (!movie) {
+    return <h1></h1>;
+  }
+
+  return <h1>movie page {movie.title}</h1>;
 };
