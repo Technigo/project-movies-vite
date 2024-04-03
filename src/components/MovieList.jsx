ca11ef94ee364a70297775025bab548ceddd17c6
Maria-source-cell <manuelabirnat@gmail.com>
2024-04-03 23:48:11 +0200

    styling in progres for main page

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -8,7 +8,7 @@ const MovieList = () => {
 	const [listChoice, setListChoice] = useState("movie/upcoming");
 
 	useEffect(() => {
-		fetch(`https://api.themoviedb.org/3/${listChoice}?api_key=45808b94c818eebf69530d0b48e42b20&language=en-US&page=1`)
+		fetch(`https://api.themoviedb.org/3/${listChoice}?api_key=45808b94c818eebf69530d0b48e42b20`)
 			.then((response) => response.json())
 			.then((data) => {
 				const results = data.results;
@@ -18,25 +18,51 @@ const MovieList = () => {
 			.catch((error) => console.error('Error fetching popular movies:', error))
 	}, [listChoice]);
 
+
 	return (
-		<div className="movie-list-container">
-			<section className="home-page">
+		<div className="movie-list">
+			<div className="options-container">
+				<button
+					className={`option-button ${listChoice === "movie/popular" ? "active" : ""}`}
+					onClick={() => setListChoice("movie/popular")}
+				>
+					Popular Movies
+				</button>
+				<button
+					className={`option-button ${listChoice === "movie/top_rated" ? "active" : ""}`}
+					onClick={() => setListChoice("movie/top_rated")}
+				>
+					Top Rated Movies
+				</button>
+				<button
+					className={`option-button ${listChoice === "movie/upcoming" ? "active" : ""}`}
+					onClick={() => setListChoice("movie/upcoming")}
+				>
+					Upcoming Movies
+				</button>
+			</div>
+
+			<div className="movie-container">
 				{movieList.map((movie, index) => (
 					<Link key={index} to={`/movies/${movie.id}`}>
 						<div className="movie-card">
-							<img
-								src={`http://image.tmdb.org/t/p/w342/${movie.poster_path}`}
-								alt="movie-image"
-								className="movie-poster"
-							/>
-							<h1>{movie.title}</h1>
-							<div className="description">
-								<p>Release {movie.release_date}</p>
+							<div className="image-container">
+								<img
+									src={`http://image.tmdb.org/t/p/w342/${movie.poster_path}`}
+									alt="movie-image"
+									className="movie-poster"
+								/>
+								<div className="overlay">
+									<div className="description">
+										<h1>{movie.title}</h1>
+										<p>Release {movie.release_date}</p>
+									</div>
+								</div>
 							</div>
 						</div>
 					</Link>
 				))}
-			</section>
+			</div>
 		</div>
 	);
 };
