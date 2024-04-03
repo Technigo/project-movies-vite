89933e7872552369fb1b7e37bac23d6001208a13
Johanna Catalini Smith <johannacatalini@gmail.com>
2024-04-03 21:34:39 +0200

    added movie poster, title, votes, shadow

--- src/MoviePage.jsx
+++ src/MoviePage.jsx
@@ -19,9 +19,36 @@ export const MoviePage = () => {
   }
 
   return (
-    <div>
+    <div className="MoviePage">
+      <img
+        className="MoviePage-backgroundImage"
+        src={`https://image.tmdb.org/t/p/w1280/${movie.backdrop_path}`}
+        alt=""
+      />
+      <div className="MoviePage-info">
+        <img
+          className="MoviePage-poster"
+          src={`https://image.tmdb.org/t/p/w200/${movie.poster_path}`}
+          alt=""
+        />
+
+        <div className="MoviePage-heading">
+          <span className="MoviePage-title">{movie.title}</span>{" "}
+          <span className="MoviePage-votes">
+            <img src="/star.svg" alt="" />
+            {movie.vote_average.toFixed(1)}
+          </span>
+        </div>
+
+        <div className="MoviePage-overview">{movie.overview}</div>
+      </div>
+
       <a className="MoviePage-backLink" href="/">
-        <svg className="MoviePage-backIcon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 30 30">
+        <svg
+          className="MoviePage-backIcon"
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 30 30"
+        >
           <path
             d="M27 14.5C27 7.596441 21.4035594 2 14.5 2S2 7.596441 2 14.5 7.5964406 27 14.5 27 27 21.403559 27 14.5zm-19.3388348-.353553l7.4852814-7.485282c.1952622-.195262.5118446-.195262.7071068 0l2.1213203 2.121321c.1952622.195262.1952622.511844 0 .707106L12.9644661 14.5l5.0104076 5.010408c.1952622.195262.1952622.511844 0 .707106l-2.1213203 2.121321c-.1952622.195262-.5118446.195262-.7071068 0l-7.4852814-7.485282c-.19799-.19799-.197989-.509117 0-.707106z"
             fill="#fff"
@@ -30,7 +57,6 @@ export const MoviePage = () => {
         </svg>
         Movies
       </a>
-      <h1>movie page {movie.title}</h1>
     </div>
   );
 };
