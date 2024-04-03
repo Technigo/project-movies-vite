6132e3669bafa0019ff507481093c5f301b4676c
Martin Jönsson <martin.t.joensson@gmail.com>
2024-04-03 08:55:19 +0200

    Passed fetched data to details.
    Co-authored-by: Linda <linda-f@users.noreply.github.com>

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,37 @@
-import { BrowserRouter, Routes } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { BrowserRouter } from "react-router-dom";
 import { MovieRoutes } from "./routes/MovieRoutes";
 import { Header } from "./components/Header";
 export const App = () => {
-  
+  const [loading, setLoading] = useState(true);
+  const [data, setData] = useState();
+  const [API_END, setAPI_END] = useState("popular");
+  const API_KEY = "5ebb37047349cdb54acf91b06e7b7839";
+  const API_URL = `https://api.themoviedb.org/3/movie/${API_END}?api_key=${API_KEY}&language=en-US&page=1`;
+
+  useEffect(() => {
+    fetch(API_URL)
+      .then((result) => result.json())
+      .then((json) => {
+        setData(json);
+        setLoading(false);
+      })
+      .catch((error) => {
+        console.error(error);
+        setLoading(false);
+      });
+  }, []);
+
+  if (loading) {
+    return <p>Loading...</p>;
+  }
+
   return (
     <>
       <BrowserRouter>
         <Header />
         <main>
-          <Routes>{MovieRoutes}</Routes>
+          <MovieRoutes data={data} />
         </main>
       </BrowserRouter>
     </>
--- src/routes/MovieRoutes.jsx
+++ src/routes/MovieRoutes.jsx
@@ -1,12 +1,21 @@
-import { Route } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import { MovieGrid } from "../pages/MovieGrid";
 import { MovieDetails } from "../pages/MovieDetails";
 import { NotFound } from "../pages/NotFound";
+import PropTypes from "prop-types";
 
-export const MovieRoutes = (
-  <>
-        <Route path="/" element={<MovieGrid />} />
-        <Route path="*" element={<NotFound/>} />
-        <Route path="/movie/:slug" element={<MovieDetails/>}/>
-  </>
-);
+export const MovieRoutes = ({ data }) => {
+  console.log("Routes data: ", data);
+
+  return (
+    <Routes>
+      <Route path="/" element={<MovieGrid data={data} />} />
+      <Route path="*" element={<NotFound />} />
+      <Route path="/movie/:slug" element={<MovieDetails data={data} />} />
+    </Routes>
+  );
+};
+
+MovieRoutes.propTypes = {
+  data: PropTypes.object,
+};
