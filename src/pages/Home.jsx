935e7ff861b9d00ea5e018b72f43997ab4088f43
fridaforser <fridaforser@gmail.com>
2024-04-04 17:21:38 +0200

    Added home container to display all movies in a flex wrap

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,6 @@
 import { Link } from "react-router-dom";
 import { useEffect, useState } from "react";
+import "./Home.css";
 
 export const Home = () => {
   const [movies, setMovies] = useState([]);
@@ -21,15 +22,24 @@ export const Home = () => {
   }, []);
 
   return (
-    <div>
-      {movies.map((movie) => (
-        <div key={movie.id}>
-          <p>{movie.title}</p>
-          <Link key={movie.id} to={`/movie/${movie.id}`}>
-            <img src={`https://image.tmdb.org/t/p/w342${movie.poster_path}`} />
-          </Link>
-        </div>
-      ))}
+    <div className="home-container">
+      <div className="movie-boxes">
+        {movies.map((movie) => (
+          <div className="movie-box" key={movie.id}>
+            <div className="hover-info">
+              <h1 className="title">{movie.title}</h1>
+              <h4 className="release-date">Released {movie.release_date}</h4>
+            </div>
+            <Link key={movie.id} to={`/movie/${movie.id}`}>
+              <img
+                className="movie-poster"
+                src={`https://image.tmdb.org/t/p/w780${movie.poster_path}`}
+                alt="poster"
+              />
+            </Link>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
