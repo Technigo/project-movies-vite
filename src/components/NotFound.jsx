b6cfd4796221e69df643af77fcb0bd398c91b2ec
Caracal23 <154092424+Caracal23@users.noreply.github.com>
2024-04-04 09:02:06 +0200

    adding a return button to the "not found page"

--- src/components/NotFound.jsx
+++ src/components/NotFound.jsx
@@ -1,3 +1,17 @@
+import { Link } from "react-router-dom";
+import arrow from "/arrow.svg";
+
 export const NotFound = () => {
-  return <div>NotFound</div>;
+  return (
+    <div className="movie-component">
+      <div className="button">
+        <Link to="/">
+          <img src={arrow} id="go-back" />
+        </Link>
+        <Link to="/">
+          <h4>Movies</h4>
+        </Link>
+      </div>
+    </div>
+  );
 };
