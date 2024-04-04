7cd9c3e69586227361bb148311f09a0f5b5305db
Martin Jönsson <martin.t.joensson@gmail.com>
2024-04-04 20:57:47 +0200

    Logo update, updated transitions.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,18 +1,23 @@
 import { useRef } from "react";
 import { NavLink } from "react-router-dom";
-import { FaBars, FaTimes } from "react-icons/fa";
+import { FaBars, FaTimes, FaFilm } from "react-icons/fa";
 import "./Header.css";
 
 export const Header = () => {
   const navRef = useRef();
+  const btnRef = useRef();
 
   const showNavbar = () => {
     navRef.current.classList.toggle("responsive-navbar");
+    btnRef.current.classList.toggle("hide-button");
   };
 
   return (
     <header className="nav-bar">
-      <h3>Cinema</h3>
+      <h3 className="film-logo">
+        <FaFilm />
+        CineCheck
+      </h3>
       <div className="controls" ref={navRef}>
         <NavLink className="nav-link" to="/now_playing">
           Now Playing
@@ -30,7 +35,11 @@ export const Header = () => {
           <FaTimes />
         </button>
       </div>
-      <button className="nav-button" onClick={showNavbar}>
+      <button
+        ref={btnRef}
+        className="nav-button nav-burger-button"
+        onClick={showNavbar}
+      >
         <FaBars />
       </button>
     </header>
