9121148f2ce570d760329d80cd7b1eb4e4845e74
Johanna Billingskog Nyberg <johanna@billingskognyberg.se>
2024-04-04 13:23:24 +0200

    Added pages.

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 // import { MovieCard } from "./components/MovieCard";
 import { MovieDetails } from "./components/MovieDetails";
-import { MoviesPage } from "./MoviesPage";
+import { MoviesPage } from "./pages/MoviesPage";
 
 const App = () => {
   return (
---
+++ src/pages/MovieDetailsPage.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+//Should fetch data from API
+export const MovieDetailsPage = () => {
+  //Should render MovieDetails-component
+  return <div>MovieDetailsPage</div>;
+};
