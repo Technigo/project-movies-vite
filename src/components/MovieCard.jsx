cf07c1a39248dc786f3914d3d75375b9edb1ddf5
Johanna Billingskog Nyberg <johanna@billingskognyberg.se>
2024-04-04 13:39:15 +0200

    Added Link to MovieCard component.

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,14 +1,20 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import "./MovieCard.css";
 
 export const MovieCard = ({ title, rating, poster_path }) => {
   return (
-    <div className="movie-card">
-      <img src={`https://image.tmdb.org/t/p/w300/${poster_path}`} alt={title} />
-      <div className="movie-card-info">
-        <h2>{title}</h2>
-        <p>⭐ {rating}</p>
+    <Link to="/movie">
+      <div className="movie-card">
+        <img
+          src={`https://image.tmdb.org/t/p/w300/${poster_path}`}
+          alt={title}
+        />
+        <div className="movie-card-info">
+          <h2>{title}</h2>
+          <p>⭐ {rating}</p>
+        </div>
       </div>
-    </div>
+    </Link>
   );
 };
