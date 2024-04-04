d7df5a783a1e472216f1b48d7e397dac479a1c00
fridaforser <fridaforser@gmail.com>
2024-04-04 17:21:10 +0200

    Added movie info container with background image and movie details

--- src/pages/MoviePages.jsx
+++ src/pages/MoviePages.jsx
@@ -1,10 +1,11 @@
 import { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
+import { useParams, Link } from "react-router-dom";
+import arrow from "./assets/arrow.png";
+import "./MoviePages.css";
 
 export const MoviePages = () => {
   const [movieInfo, setMovieInfo] = useState();
-  const params = useParams();
-  const movie_id = params.movieID;
+  const movie_id = useParams().movieID;
   const movieURL = `https://api.themoviedb.org/3/movie/${movie_id}?api_key=e58923a817ec3772f69470b86e481a40&language=en-US`;
 
   useEffect(() => {
@@ -25,13 +26,34 @@ export const MoviePages = () => {
     return <div>Loading...</div>;
   }
 
+  let ratingRound = movieInfo.vote_average.toFixed(1);
+
   return (
-    <div>
-      <img
-        src={`https://image.tmdb.org/t/p/w780${movieInfo.backdrop_path}`}
-      ></img>
-      <p>{movieInfo.title}</p>
-      <p>{movieInfo.release_date}</p>
+    <div className="info-container">
+      <div className="home-button">
+        <Link className="home" to="/">
+          <img className="icon" src={arrow}></img> Home
+        </Link>
+      </div>
+      <div
+        className="info-backdrop"
+        style={{
+          "--img": `url(https://image.tmdb.org/t/p/w1280${movieInfo.backdrop_path}), linear-gradient(#000000, #ffffff)`,
+        }}
+        alt="backdrop"
+      ></div>
+      <div className="movie-info-box">
+        <img
+          className="info-poster"
+          src={`https://image.tmdb.org/t/p/w342${movieInfo.poster_path}`}
+          alt="poster"
+        ></img>
+        <div className="movie-info">
+          <h2 className="info-rating">⭐ {ratingRound}</h2>
+          <h1 className="info-title">{movieInfo.title}</h1>
+          <h3 className="info-overview">{movieInfo.overview}</h3>
+        </div>
+      </div>
     </div>
   );
 };
