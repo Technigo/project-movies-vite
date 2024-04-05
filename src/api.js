afa118fefc766b010de7146d753fdcfe3fec982c
Mai Kanetaka <maikanetaka@gmail.com>
2024-04-05 12:01:01 +0200

    Create "api.js" and fetch data for PopularMovies and MovieDetails

---
+++ src/api.js
@@ -0,0 +1,19 @@
+const API_BASE_URL = "https://api.themoviedb.org/3/movie";
+const API_KEY = import.meta.env.VITE_APP_API_KEY;
+
+export const fetchPopularMovies = async () => {
+  const response = await fetch(`${API_BASE_URL}/movie/popular?api_key=${API_KEY}&language=en-US&page=1`)
+  if (!response.ok) {
+    throw new Error ("Failed to load popular movies")
+  } return await response.json();
+}
+
+export const fetchMovieDetails = async (movieId) => {
+  const response = await fetch(
+    `${API_BASE_URL}/movie/${movieId}?api_key=${API_KEY}&language=en-US`
+  );
+  if (!response.ok) {
+    throw new Error("Failed to load movie details");
+  }
+  return await response.json();
+};
