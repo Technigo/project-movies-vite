f9f86d759d54f84e2a6197379f520b9ba9267418
Johanna Billingskog Nyberg <johanna@billingskognyberg.se>
2024-04-05 11:03:14 +0200

    Added media queries for desktop and extra large screens on MovieDetails.

--- src/components/MovieDetails.jsx
+++ src/components/MovieDetails.jsx
@@ -14,42 +14,43 @@ export const MovieDetails = ({
   imdb,
 }) => {
   return (
-    <div className="movie-details-page">
+    <div>
       <img
         className="backdrop"
         src={`https://image.tmdb.org/t/p/w1280${backdrop}`}
         alt={title}
       />
       {/* <div className="movie-details-content"> */}
-      <div className="movie-details-info">
+      <div className="movie-details-content">
         <img
           className="poster"
           src={`https://image.tmdb.org/t/p/w1280${poster_path}`}
         />
-        <p className="rating">⭐ {rating}</p>
-        <h2>{title}</h2>
-        <h3>{tagline}</h3>
-        <div className="movie-genre-list">
-          <ul>
-            {genres.map((genre) => (
-              <li key={genre.id}>/ {genre.name}</li>
-            ))}
-          </ul>
+        <div className="movie-details-text">
+          <p className="rating">⭐ {rating}</p>
+          <h2>{title}</h2>
+          <h3>{tagline}</h3>
+          <div className="movie-genre-list">
+            <ul>
+              {genres.map((genre) => (
+                <li key={genre.id}>/ {genre.name}</li>
+              ))}
+            </ul>
+          </div>
+          <p>{overview}</p>
+          <a
+            href={`https://www.imdb.com/title/${imdb}`}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <button className="read-more-button">Read more</button>
+          </a>
+          <p className="release-date">Released {release_date}</p>
+          <Link to={`/movie`}>
+            <button className="back-button">Go back</button>
+          </Link>
         </div>
-        <p>{overview}</p>
-        <a
-          href={`https://www.imdb.com/title/${imdb}`}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <button className="read-more-button">Read more</button>
-        </a>
-        <p className="release-date">Released {release_date}</p>
-        <Link to={`/movie`}>
-          <button className="back-button">Go back</button>
-        </Link>
       </div>
     </div>
-    // </div>
   );
 };
