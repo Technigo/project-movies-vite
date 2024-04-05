348f9d0b7743cf987e2e45ba75b9a2d9589aeced
Your El1an3 <eliane_w@yahoo.com>
2024-04-05 16:23:50 +0200

    fixed toFixed(1)

--- src/components/Description.jsx
+++ src/components/Description.jsx
@@ -2,29 +2,14 @@
 import "../pages/MovieDetails.css";
 
 export const Description = ({ movieName, vote, overview }) => {
-  const formatVote = Math.round(vote * 10) / 10;
+  const formatVote = vote?.toFixed(1); //optional chaining operator (?.), checks that the value is not null or undefined
   return (
     <div className="details">
       <h2>
-        <span className="title">{movieName}</span>{" "}
-        <span className="rating">
-          <span>⭐️</span> <span>{formatVote}</span>
-        </span>
+        <span className="title">{movieName}</span>
+        <span className="rating">⭐️ {formatVote}</span>
       </h2>
       <p>{overview}</p>
     </div>
   );
 };
-
-// export const Description = ({ movieName, vote, overview }) => {
-//   const formatVote = vote.toFixed(1);
-//   return (
-//     <div className="details">
-//       <h2>
-//         <span className="title">{movieName}</span>{" "}
-//         <span className="rating">⭐️ {formatVote}</span>
-//       </h2>
-//       <p>{overview}</p>
-//     </div>
-//   );
-// };
