79e42b609c3d6327f96470f8446f8f0a9ad88806
Johanna Billingskog Nyberg <johanna@billingskognyberg.se>
2024-04-05 16:30:49 +0200

    Added netlify.toml file.

--- src/pages/MovieDetailsPage.jsx
+++ src/pages/MovieDetailsPage.jsx
@@ -1,7 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { MovieDetails } from "../components/MovieDetails";
-import { NotFoundPage } from "./NotFoundPage";
 
 export const MovieDetailsPage = () => {
   const { id } = useParams();
