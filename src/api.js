d3dcb4c1b95776fec0ccbc3bef7cdf525471c77f
Mai Kanetaka <maikanetaka@gmail.com>
2024-04-05 23:13:30 +0200

    Fix bugs on api.js

--- src/api.js
+++ src/api.js
@@ -2,15 +2,18 @@ const API_BASE_URL = "https://api.themoviedb.org/3/movie";
 const API_KEY = import.meta.env.VITE_APP_API_KEY;
 
 export const fetchPopularMovies = async () => {
-  const response = await fetch(`${API_BASE_URL}/popular?api_key=${API_KEY}&language=en-US&page=1`)
+  const response = await fetch(
+    `${API_BASE_URL}/popular?api_key=${API_KEY}&language=en-US&page=1`
+  );
   if (!response.ok) {
-    throw new Error ("Failed to load popular movies")
-  } return await response.json();
-}
+    throw new Error("Failed to load popular movies");
+  }
+  return await response.json();
+};
 
 export const fetchMovieDetails = async (movieId) => {
   const response = await fetch(
-    `${API_BASE_URL}/movie/${movieId}?api_key=${API_KEY}&language=en-US`
+    `${API_BASE_URL}/${movieId}?api_key=${API_KEY}&language=en-US`
   );
   if (!response.ok) {
     throw new Error("Failed to load movie details");
