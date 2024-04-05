c0380bcbf6ac4fa41d311def2e5306abc909fd30
ohitsnathalie <152981339+ohitsnathalie@users.noreply.github.com>
2024-04-06 00:18:12 +0200

    fixed 404 and added styling to it

--- src/components/404/NotFound.jsx
+++ src/components/404/NotFound.jsx
@@ -1,13 +1,21 @@
-import { useRouteError } from "react-router-dom"
+import { Link } from "react-router-dom"
+import notFoundImg from "/pexels-tima-miroshnichenko-7991428-large.jpg"
 import "./NotFound.css"
 
 export const NotFound = () => {
-  const error = useRouteError()
-  console.error(error)
-  return (
-    <div>
-      <h1>Oups!</h1>
-      <p>{error.statusText || error.message}</p>
-    </div>
-  )
+	return (
+		<div
+			className='not-found-container'
+			style={{
+				backgroundImage: `linear-gradient(rgba(0, 0, 0, 0) 20%, rgb(0, 0, 0) 100%), url(${notFoundImg})`,
+				backgroundRepeat: "no-repeat",
+				backgroundSize: "cover",
+			}}>
+			<h1>Oups!</h1>
+			<p>Nothing to see here</p>
+			<div className='link-back'>
+				<Link to='/'>Back to the movies</Link>
+			</div>
+		</div>
+	)
 }
