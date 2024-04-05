5b6e3a20c61fb3c0838910ce478a8196f49f29b9
Sofia Johansson <153744881+sofia32057@users.noreply.github.com>
2024-04-05 22:21:08 +0200

    Update - added genres to details page

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -2,11 +2,15 @@ import "./Header.css"
 import { NavLink } from "react-router-dom"
 
 export const Header = () => {
-	return (
-		<header>
-			<nav>
-				<NavLink to='/'>Movies</NavLink>
-			</nav>
-		</header>
-	)
+  return (
+    <header>
+      <nav>
+        <NavLink
+          to="/"
+          className="home">
+          HOME ›
+        </NavLink>
+      </nav>
+    </header>
+  )
 }
