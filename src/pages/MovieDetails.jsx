abddcefa889fe3058e22fcf0fb42c39e0287ee92
Your El1an3 <eliane_w@yahoo.com>
2024-04-05 18:55:14 +0200

    added not found page

--- src/pages/MovieDetails.jsx
+++ src/pages/MovieDetails.jsx
@@ -6,26 +6,44 @@ import { Poster } from "../components/Poster";
 import { Description } from "../components/Description";
 import { Background } from "../components/Background";
 import { BackArrow } from "../components/BackArrow";
+import { NotFound } from "./NotFound";
 
 import "./MovieDetails.css";
 
 export const MovieDetails = () => {
   const [details, setDetails] = useState([]);
+  const [notFound, setNotFound] = useState(false);
   const { movie_id } = useParams();
 
   const API_KEY = "f34e76ca0c4c61e8906dd3e22b0fe2af";
   const API_LANG = "en-US";
-
   useEffect(() => {
     fetch(
       `https://api.themoviedb.org/3/movie/${movie_id}?api_key=${API_KEY}&language=${API_LANG}`
     )
-      .then((res) => res.json())
+      .then((res) => {
+        if (res.ok) {
+          return res.json();
+        } else {
+          throw new Error("Movie details not found");
+        }
+      })
       .then((data) => setDetails(data))
-      .catch((error) => console.error("Error fetching details:", error));
+      .catch((error) => {
+        console.error("Error fetching details:", error);
+        setNotFound(true); // Set notFound state to true
+      });
   }, [movie_id]);
 
-  if (!details) return <p>Loading...</p>;
+  // If details are not found, return notFound page
+  if (notFound) {
+    return <NotFound />;
+  }
+
+  // If details are not loaded yet, show loading message
+  if (!details || Object.keys(details).length === 0) {
+    return <p>Loading...</p>;
+  }
 
   return (
     <section className="detailContainer">
