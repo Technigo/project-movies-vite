4de0d24b2a58ccd6e75d680aefc02c44b2b2492f
Mai Kanetaka <maikanetaka@gmail.com>
2024-04-05 20:16:03 +0200

    Add "MovieDetail.jsx" to fetch and display details

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import MovieList from "../components/MovieList"
-import MovieDetails from "../components/MovieDetails"
+import { MovieList } from "./components/MovieList"
+import { MovieDetail } from "./components/MovieDetail"
 
 export const App = () => {
   <BrowserRouter>
     <Routes>
       <Route path="/" element={<MovieList />} />
-      <Route path="/movies/:id" element={<MovieDetails />} />
+      <Route path="/movies/:id" element={<MovieDetail />} />
     </Routes>
   </BrowserRouter>
 }
