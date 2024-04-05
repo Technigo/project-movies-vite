2c28160b979327687cebe5ea1aea035e3adfff7b
SofieFerrari <sofiestrahl@gmail.com>
2024-04-05 21:46:32 +0200

    got backgroundpic in place for detaillist

--- src/components/DetailList.jsx
+++ src/components/DetailList.jsx
@@ -1,4 +1,5 @@
 import { Link } from 'react-router-dom'
+import '../styling/DetailList.css'
 
 export const DetailList = ({ detailData }) => {
 	console.log('check data in DetailList', detailData)
@@ -12,13 +13,14 @@ export const DetailList = ({ detailData }) => {
 						<div>Placeholder for back icon</div>
 					</div>
 				</Link>
-				{/* <div
+				<div
 					className="movie-details"
 					style={{
-						backgroundImage: `url(https://media.themoviedb.org/t/p/w1280_and_h720_multi_faces/${detailData.backdrop_path})`,
+						backgroundImage: `url(https://media.themoviedb.org/t/p/w1280${detailData.backdrop_path})`,
 						backgroundRepeat: 'no-repeat',
 						backgroundSize: 'cover',
-					}}> */}
+            minWidth: "100VW",
+					}}>
 					<div className="details-card">
 						<div className="details-image">
 							<img src="" alt=""></img>
@@ -30,7 +32,7 @@ export const DetailList = ({ detailData }) => {
 						</div>
 					</div>
 				</div>
-			{/* </div> */}
+			</div>
 			{/* ))} */}
 		</>
 	)
