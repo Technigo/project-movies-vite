8eabe68f9d74ccff6115c4b167dd58e6d02b3abe
schouenkes <m.schouenke@gmail.com>
2024-04-07 00:00:53 +0200

    styled 404 page

--- src/pages/PageNotFound.jsx
+++ src/pages/PageNotFound.jsx
@@ -1,7 +1,11 @@
+import "./PageNotFound.css";
+
 export const PageNotFound = () => {
   return (
     <div className="page-not-found">
-      <p>Oops .. we can&apos;t seem to find the page you&apos;re looking for.</p>
+      <div className="text-box">
+        <h1>Oups ..</h1> <p>we can&apos;t seem to find the page you&apos;re looking for.</p>
+      </div>
     </div>
   );
 };
