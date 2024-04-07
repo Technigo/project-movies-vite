06dc2a30800c16b9827308f6679ed93ad3aafde9
Mai Kanetaka <maikanetaka@gmail.com>
2024-04-07 21:14:39 +0200

    Add & style animation in NotFound.jsx & CSS

--- src/components/NotFound/NotFound.jsx
+++ src/components/NotFound/NotFound.jsx
@@ -1,21 +1,23 @@
-import Lottie from "react-lottie"
-import notFoundAnimation from "../../assets/animations/not-found.json"
-import styles from "./NotFound.module.css"
+import Lottie from "react-lottie";
+import notFoundAnimation from "../../assets/animations/not-found.json";
+import styles from "./NotFound.module.css";
 
 export const NotFound = () => {
   const defalutOptions = {
     loop: true,
     autoplay: true,
-    animation: notFoundAnimation,
+    animationData: notFoundAnimation,
     rendererSettings: {
-      preserveAspectRatio: "xMidYMid slice"
-    }
-  }
+      preserveAspectRatio: "xMidYMid slice",
+    },
+  };
 
   return (
-    <div className={StyleSheet.animationContainer}>
-      <Lottie options={defalutOptions} />
-      <p className={styles.notFoundMessage}>Page not found</p>
+    <div className={styles.notFoundContainer}>
+      <div className={styles.notFoundAnimationContainer}>
+        <Lottie options={defalutOptions} height={400} width={400} />
+      </div>
+      <p className={styles.notFoundMessage}>Opps... Page not found</p>
     </div>
   );
-}
+};
