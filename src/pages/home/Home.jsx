b0d08633589430e9c80fa01d9b0983245a7bc6ba
Yifan-858 <133569248+Yifan-858@users.noreply.github.com>
2024-04-07 16:14:39 +0200

    add search error in Home

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -8,8 +8,7 @@ export const Home = () => {
   const apiKey = "195790d926bf4d38c02251685a7c5f5e";
   const [movieHomeData, setMovieHomeData] = useState("");
   const [searchQuery, setSearchQuery] = useState("");
-
-  //   const [searchError, setSearchError] = useState(false);
+  const [searchError, setSearchError] = useState(false);
 
   const [isLoading, setIsLoading] = useState(true);
 
@@ -42,13 +41,16 @@ export const Home = () => {
   const searchMovie = async () => {
     try {
       const response = await fetch(
-        `https://api.themoviedb.org/3/search/movie?query=${searchQuery}&api_key=${apiKey}&include_adult=false&language=en-US&page=2`
+        `https://api.themoviedb.org/3/search/movie?query=${searchQuery}&api_key=${apiKey}&include_adult=false&language=en-US&page=1`
       );
       if (response.ok) {
         const data = await response.json();
         if (data.results.length > 0) {
           const movieId = data.results[0].id;
           return `/movie/${movieId}`;
+        } else {
+          setSearchError(true);
+          throw new Error("Failed at fetch the movie list");
         }
       }
     } catch (error) {
@@ -89,6 +91,13 @@ export const Home = () => {
                 onKeyDown={handleSearchEnter}
                 placeholder="Search for a movie by name..."
               />
+              <div className="search-error-message-container">
+                {searchError && (
+                  <p className="search-error-message">
+                    Can&#39;t find any result. Please check the name again...
+                  </p>
+                )}{" "}
+              </div>
             </div>
           </form>
         </div>
