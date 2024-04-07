fcc6e4c33f65b49d2346b2dc22fa7b432c38f8a4
Mai Kanetaka <maikanetaka@gmail.com>
2024-04-07 20:55:01 +0200

    Add NotFound in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { MovieList } from "./components/MovieList/MovieList";
 import { MovieDetails } from "./components/MovieDetails/MovieDetails";
+import { NotFound } from "./components/NotFound/NotFound";
 
 export const App = () => {
   return (
@@ -8,6 +9,7 @@ export const App = () => {
       <Routes>
         <Route path="/" element={<MovieList />} />
         <Route path="/movies/:movieId" element={<MovieDetails />} />
+        <Route path="*" element={<NotFound />} />
       </Routes>
     </BrowserRouter>
   );
