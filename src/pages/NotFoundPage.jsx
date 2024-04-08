d7b655708f72df4129340198474b4d5b422ea067
Johanna Billingskog Nyberg <johanna@billingskognyberg.se>
2024-04-08 15:42:02 +0200

    Added styling to 404-page.

--- src/pages/NotFoundPage.jsx
+++ src/pages/NotFoundPage.jsx
@@ -1,11 +1,19 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import "./NotFoundPage.css";
+import arrowIcon from "../assets/arrow-icon.png";
 
 export const NotFoundPage = () => {
   return (
-    <div>
-      <Link to="/">Go back</Link>
-      <h1>It looks like the page your are trying to reach doesn't exist. </h1>
+    <div className="not-found-page">
+      <div className="camera-icon">🎥</div>
+      <h1>Oops..</h1>
+      <p>It looks like this page doesn't exist.</p>
+      <div className="error-back-button">
+        <Link to={`/movie`}>
+          <img src={arrowIcon} alt="Go back" />
+        </Link>
+      </div>
     </div>
   );
 };
