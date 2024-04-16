b8e223da7acb0923c9f9e84882af61d94e5f56c7
Katarina Sjölin <kat.sjolin@gmail.com>
2024-04-16 12:53:50 +0200

    fetched movie details from API

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 import { PopularList } from "./components/PopularList"
+import { Movie } from "./components/Movie"
+
 
 export function App() {
   return (
@@ -7,6 +9,7 @@ export function App() {
       <main>
         <Routes>
           <Route path="/" element={<PopularList />} />
+          <Route path="/movie/:movieId" element={<Movie />} />
         </Routes>
       </main>
     </BrowserRouter>
---
+++ src/components/Movie.jsx
@@ -0,0 +1,46 @@
+import { useState, useEffect } from "react"
+import { useParams, useNavigate } from "react-router-dom"
+
+export const Movie = () => {
+  const [movie, setMovie] = useState([])
+  const { movieId } = useParams()
+  const navigate = useNavigate()
+  const api_key = "693b375f74896f94616e9c8c846f1014"
+
+  useEffect(() => {
+    fetch(`https://api.themoviedb.org/3/movie/${movieId}?api_key=${api_key}&language=en-US&page=1`)
+    .then(response => response.json())
+    .then(data => setMovie(data))
+    .catch(error => console.error("Error fetching data", error))
+  }, [movieId])
+
+  const returnButton = () => {
+    navigate("/")
+  }
+
+  return (
+    <div
+      style={{
+        backgroundImage: `url(https://image.tmdb.org/t/p/w1280${movie.backdrop_path})`
+      }}
+    >
+      <div className="movie-container">
+        <div>
+          <button className="return-button" onClick={returnButton}>
+            Movies
+          </button>
+        </div>
+        <div className="movie-info">
+          <h2>{movie.title}</h2>
+          <h3>⭐️{Math.round(movie.vote_average * 10) / 10}</h3>
+          <p>{movie.overview}</p>
+        </div>
+        <img
+          className="poster-image"
+          src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
+          alt={`${movie.title} Poster`}
+        />
+      </div>
+    </div>
+  )
+}
