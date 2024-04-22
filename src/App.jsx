0e97fb25feb400a194b26ef3428a68adf1037179
Looziolooz <lorenzo.dastoli@gmail.com>
2024-04-22 11:03:12 +0200

    data component

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,41 @@
-export const App = () => {
-  return <div>Find me in src/app.jsx!</div>;
+import { useState, useEffect } from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'; 
+import PopularList from './assets/components/PopularList/PopularList';
+import MovieDetail from './assets/components/MovieDetail/MovieDetail';
+import NotFound from './assets/components/NotFound/NotFound';
+
+
+
+
+const App = () => {
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    fetchMovies();
+  }, []);
+
+  const fetchMovies = async () => {
+    try {
+      const response = await fetch('data/movies.json');
+      if (!response.ok) {
+        throw new Error('Failed to fetch movies');
+      }
+      const data = await response.json();
+      setMovies(data.results);
+    } catch (error) {
+      console.error('Error fetching movies:', error);
+    }
+  };
+
+  return (
+    <Router>
+      <Routes> 
+        <Route exact path="/" element={<PopularList movies={movies} />} />
+        <Route path="/movies/:id" element={<MovieDetail />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </Router>
+  );
 };
+
+export default App;
