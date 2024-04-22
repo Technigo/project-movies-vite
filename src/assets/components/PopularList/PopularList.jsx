ed3416ac143c30db314fdeb57c9eafdd46982b94
Looziolooz <lorenzo.dastoli@gmail.com>
2024-04-22 11:05:11 +0200

    popularlist

---
+++ src/assets/components/PopularList/PopularList.jsx
@@ -0,0 +1,37 @@
+import { Link } from "react-router-dom";
+import PropTypes from "prop-types";
+import "./PopularList.css"; // Import CSS file
+
+const PopularList = ({ movies }) => {
+  return (
+    <div className="popular-list">
+      {movies.map((movie) => (
+        <Link to={`/movies/${movie.id}`} key={movie.id} className="movie-link">
+          <div className="film-card">
+            <div className="film-img">
+              <img src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} alt={movie.title} />
+            </div>
+            <div className="film-card-text-wrap">
+              <h2 className="title">{movie.title}</h2>
+              <p className="movie-release-date">Release Date: {movie.release_date}</p>
+            </div>
+          </div>
+        </Link>
+      ))}
+    </div>
+  );
+};
+
+PopularList.propTypes = {
+  movies: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.number.isRequired,
+      title: PropTypes.string.isRequired,
+      poster_path: PropTypes.string.isRequired,
+      release_date: PropTypes.string.isRequired,
+      overview: PropTypes.string.isRequired,
+    })
+  ).isRequired,
+};
+
+export default PopularList;
