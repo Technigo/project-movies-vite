fafb7b80fd92488ada2bedb3e3bfcaa00a9980fc
Looziolooz <lorenzo.dastoli@gmail.com>
2024-04-23 22:23:20 +0200

    notfoundpage css

--- src/assets/Pages/NotFoundPage.jsx
+++ src/assets/Pages/NotFoundPage.jsx
@@ -1,25 +1,11 @@
 import { Link } from "react-router-dom";
-
-const notFoundPageStyles = {
-    display: "flex",
-    flexDirection: "column",
-    justifyContent: "center",
-    alignItems: "center",
-    minHeight: "100vh",
-    backgroundColor: "black",
-    color: "white",
-};
-
-const linkStyles = {
-    color: "white",
-    marginTop: "20px"
-};
+import "./NotFound.css"; // Importing CSS file
 
 export const NotFoundPage = () => {
-    return (
-        <div style={notFoundPageStyles}>
-            <h1>Oh oh, you are leaving The Movie Zone! Hold on for a second.</h1>
-            <Link to="/" style={linkStyles}>Go back to the zone</Link>
-        </div>
-    );
+  return (
+    <div className="not-found-page">
+      <h1>Oh oh, you are leaving The Movie Zone! Hold on for a second.</h1>
+      <Link to="/" className="link-styles">Go back to the zone</Link>
+    </div>
+  );
 };
