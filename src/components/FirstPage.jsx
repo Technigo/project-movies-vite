5e294b206a873637b21fa81ba15e6ab7e1f3fde0
SofieFerrari <sofiestrahl@gmail.com>
2024-06-20 13:50:53 +0200

    added env-file and toml-file for deploy

--- src/components/FirstPage.jsx
+++ src/components/FirstPage.jsx
@@ -1,17 +1,13 @@
-import { useState, useEffect } from 'react'
-import { PopularList } from './PopularList.jsx'
-import { Loader } from './Loader'
+import { useState, useEffect } from "react"
+import { PopularList } from "./PopularList.jsx"
+import { Loader } from "./Loader"
 
 export const Movies = () => {
 	const [movieData, setMovieData] = useState(null)
 	const [isLoading, setIsLoading] = useState(true)
 
-	const BASE_URL = 'https://api.themoviedb.org/3/movie/'
-	const API_KEY = '23ace2b0c27910f5cec13f5bdb014044'
-	// const urlMovies = `${BASE_URL}popular?api_key=${API_KEY}&language=en-US&page=1`
-
-	const testUrl =
-		'https://api.themoviedb.org/3/movie/popular?api_key=23ace2b0c27910f5cec13f5bdb014044&language=en-US&page=1'
+	const apiEnv = import.meta.env.VITE_OPENDB_KEY
+	const testUrl = `https://api.themoviedb.org/3/movie/popular?api_key=${apiEnv}&language=en-US&page=1`
 
 	useEffect(() => {
 		fetch(testUrl)
@@ -24,7 +20,7 @@ export const Movies = () => {
 				console.error(error)
 			})
 	}, [testUrl])
-	console.log('moviedata firstpage', movieData)
+	console.log("moviedata firstpage", movieData)
 	return (
 		<>
 			{isLoading && <Loader />}
