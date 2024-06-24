263d456f3ad33bf5fdc5b07def40684899c6cd6a
Looziolooz <lorenzo.dastoli@gmail.com>
2024-06-24 03:44:47 +0200

    updated

--- src/assets/Pages/MovieList.jsx
+++ src/assets/Pages/MovieList.jsx
@@ -1,3 +1,4 @@
+/* global process */
 import { useState, useEffect } from "react";
 import { Oval } from "react-loader-spinner";
 import { MovieListCard } from "../components/MovieListCard/MovieListCard"; // Import the MovieListCard component
@@ -5,61 +6,58 @@ import "../components/MovieListCard/MovieListCard.css";
 import "./MovieList.css"; // Import the styles for MovieList
 
 export const MovieList = () => {
-    const [list, setList] = useState([]);
-    const [loading, setLoading] = useState(true);
+  const [list, setList] = useState([]);
+  const [loading, setLoading] = useState(true);
 
-    const api_key = "76e3c2b7ced1315dc3c5a14d30d8b12f";
+  useEffect(() => {
+    const fetchMovies = async () => {
+      try {
+        const response = await fetch(
+          `https://api.themoviedb.org/3/movie/popular?api_key=${process.env.REACT_APP_MOVIE_DB_API_KEY}&language=en-US&page=1`
+        );
+        if (!response.ok) {
+          throw new Error("Network response was not ok");
+        }
+        const json = await response.json();
+        setList(json.results);
+        console.log(json.results);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      } finally {
+        setLoading(false);
+      }
+    };
 
-    useEffect(() => {
-        const fetchMovies = async () => {
-            try {
-                const response = await fetch(
-                    `https://api.themoviedb.org/3/movie/popular?api_key=${api_key}&language=en-US&page=1`
-                );
-                if (!response.ok) {
-                    throw new Error("Network response was not ok");
-                }
-                const json = await response.json();
-                setList(json.results);
-                console.log(json.results);
-            } catch (error) {
-                console.error("Error fetching data:", error);
-            } finally {
-                setLoading(false);
-            }
-        };
+    fetchMovies();
+  }, []);
 
-        fetchMovies();
-    }, []);
+  return (
+    <section className="movie-list-page">
+      <div className="hero-section">
+        <h1>The Movie Zone</h1>
+      </div>
 
-    return (
-        <section className="movie-list-page">
-            <div className="hero-section">
-                <h1>The Movie Zone</h1>
-            </div>
+      <div className="movie-list">
+        {/* Mapping over the list of movies to create MovieListCard components */}
+        {list.map((movie) => (
+          <MovieListCard
+            key={movie.id}
+            movieTitle={movie.title}
+            releaseDate={movie.release_date}
+            movieId={movie.id}
+            moviePoster={movie.poster_path}
+          />
+        ))}
+      </div>
 
-            <div className="movie-list">
-                {/* Mapping over the list of movies to create MovieListCard components */}
-                {list.map((movie) => (
-                    <MovieListCard
-                        key={movie.id}
-                        movieTitle={movie.title}
-                        releaseDate={movie.release_date}
-                        movieId={movie.id}
-                        moviePoster={movie.poster_path}
-                    />
-                ))}
-            </div>
-
-            {/* Displaying a loading spinner if 'loading' is true */}
-            {loading && (
-                <div className="loader-container">
-                    <Oval />
-                </div>
-            )}
-
-        </section>
-    );
+      {/* Displaying a loading spinner if 'loading' is true */}
+      {loading && (
+        <div className="loader-container">
+          <Oval />
+        </div>
+      )}
+    </section>
+  );
 };
 
 export default MovieList;
--- src/assets/Pages/SingleMovie.jsx
+++ src/assets/Pages/SingleMovie.jsx
@@ -2,81 +2,85 @@ import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import "../Pages/SingleMovie.css";
 import { BackIcon } from "../components/Icon/BackIcon";
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 
 // Your API key for accessing movie data
 const API_KEY = "76e3c2b7ced1315dc3c5a14d30d8b12f";
 
 export const SingleMovie = () => {
-    // State variable to store movie data
-    const [movie, setMovie] = useState(null);
+  // State variable to store movie data
+  const [movie, setMovie] = useState(null);
 
-    // Get the 'id' parameter from the URL using useParams
-    const { id } = useParams();
+  // Get the 'id' parameter from the URL using useParams
+  const { id } = useParams();
 
-    // Use the useEffect hook to fetch movie data when 'id' changes
-    useEffect(() => {
-        // Fetch movie data from TMDb API using the 'id' and API key
-        fetch(`https://api.themoviedb.org/3/movie/${id}?api_key=${API_KEY}&language=en-US`)
-            .then((res) => res.json()) // Convert the response to JSON format
-            .then((json) => {
-                setMovie(json); // Update the 'movie' state with the fetched data
-                console.log("data is", json);
-            })
-            .catch((error) => {
-                console.error("Error fetching data: ", error);
-            });
-    }, [id]); // Re-run this effect whenever 'id' changes
+  // Use the useEffect hook to fetch movie data when 'id' changes
+  useEffect(() => {
+    // Fetch movie data from TMDb API using the 'id' and API key
+    fetch(
+      `https://api.themoviedb.org/3/movie/${id}?api_key=${API_KEY}&language=en-US`
+    )
+      .then((res) => res.json()) // Convert the response to JSON format
+      .then((json) => {
+        setMovie(json); // Update the 'movie' state with the fetched data
+        console.log("data is", json);
+      })
+      .catch((error) => {
+        console.error("Error fetching data: ", error);
+      });
+  }, [id]); // Re-run this effect whenever 'id' changes
 
-    // Function to convert vote average to stars
-    const getStars = (rating) => {
-        const roundedRating = Math.round(rating * 10) / 10; // Round to one decimal place
-        return `⭐${roundedRating}`;
-    };
+  // Function to convert vote average to stars
+  const getStars = (rating) => {
+    const roundedRating = Math.round(rating * 10) / 10; // Round to one decimal place
+    return `⭐${roundedRating}`;
+  };
 
-    return (
-        <div className="single-movie">
-            {/* Link back to the home page */}
-            <Link to="/" className="backLink" aria-label="button back to home page">
-                <BackIcon /> Movies
-            </Link>
+  return (
+    <div className="single-movie">
+      {/* Link back to the home page */}
+      <Link to="/" className="backLink" aria-label="button back to home page">
+        <BackIcon /> Movies
+      </Link>
 
-            {/* Conditional rendering: If 'movie' data is available, display it */}
-            {movie ? (
-                <>
-                    <div className="back-drop">
-                        {/* Display a backdrop image related to the movie */}
-                        <div className="background-image">
-                            <img
-                                src={`https://image.tmdb.org/t/p/w1280/${movie.backdrop_path}`}
-                                alt={movie.title}
-                            />
-                        </div>
-                    </div>
-                    <div className="movie-info">
-                        <div className="movie-poster-container">
-                            {/* Display the movie poster */}
-                            <img
-                                src={`https://image.tmdb.org/t/p/w780/${movie.poster_path}`}
-                                alt={movie.title}
-                            />
-                        </div>
-                        <div className="movie-info-text">
-                            <div className="movie-info-heading">
-                                {/* Display the movie title with rating */}
-                                <h2>{movie.title} {getStars(movie.vote_average)}</h2>
-                            </div>
-                            <div className="movie-info-subtext">
-                                {/* Display the movie overview */}
-                                <p>{movie.overview}</p>
-                            </div>
-                        </div>
-                    </div>
-                </>
-            ) : (
-                // If 'movie' data is not available yet, display a loading message
-                <p>Loading...</p>
-            )}
-        </div>
-    );
-}
+      {/* Conditional rendering: If 'movie' data is available, display it */}
+      {movie ? (
+        <>
+          <div className="back-drop">
+            {/* Display a backdrop image related to the movie */}
+            <div className="background-image">
+              <img
+                src={`https://image.tmdb.org/t/p/w1280/${movie.backdrop_path}`}
+                alt={movie.title}
+              />
+            </div>
+          </div>
+          <div className="movie-info">
+            <div className="movie-poster-container">
+              {/* Display the movie poster */}
+              <img
+                src={`https://image.tmdb.org/t/p/w780/${movie.poster_path}`}
+                alt={movie.title}
+              />
+            </div>
+            <div className="movie-info-text">
+              <div className="movie-info-heading">
+                {/* Display the movie title with rating */}
+                <h2>
+                  {movie.title} {getStars(movie.vote_average)}
+                </h2>
+              </div>
+              <div className="movie-info-subtext">
+                {/* Display the movie overview */}
+                <p>{movie.overview}</p>
+              </div>
+            </div>
+          </div>
+        </>
+      ) : (
+        // If 'movie' data is not available yet, display a loading message
+        <p>Loading...</p>
+      )}
+    </div>
+  );
+};
