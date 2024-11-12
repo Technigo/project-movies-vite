cb9e03cabf91330fb7e2a274f632fbd364ec64fa
Zoe <zoe.lindqvist@gmail.com>
2024-11-12 20:43:25 +0100

    initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,5 @@
+import { BrowserRouter, Routes } from "react-router-dom";
+
 export const App = () => {
   return <div>Find me in src/app.jsx!</div>;
 };
---
+++ src/routes/routes.jsx
@@ -0,0 +1,4 @@
+import { Route } from "react-router-dom";
+import { Home } from "../pages/Home";
+
+export const routes = <></>;
