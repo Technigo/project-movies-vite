555bff4b4a0bc729e12a862732f1e0e79d93a016
Elina Eriksson Hult <elinaerikssonhult@gmail.com>
2024-11-13 13:35:48 +0100

    Created components folder, MovieDetails and PopularMoviesList components and added URL links and API key

---
+++ src/components/MovieDetails.jsx
@@ -0,0 +1,5 @@
+// Movies details API
+// https://api.themoviedb.org/3/movie/{movie_id}?api_key={api_key}&language=en-US
+
+// My API key
+// ff5630b14afe9dae44f59f03ba4182dc
---
+++ src/components/PopularMoviesList.jsx
@@ -0,0 +1,5 @@
+// API popular movie list
+// https://api.themoviedb.org/3/movie/popular?api_key={api_key}&language=en-US&page=1
+
+// My API key
+// ff5630b14afe9dae44f59f03ba4182dc
