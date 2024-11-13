ef257c14b2dd5c06ee6915d2494733c5149c35da
Gitte Beckmann <gittebeckmann@hotmail.com>
2024-11-13 14:51:07 +0100

    - added hover effect for movie cards

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,17 +1,40 @@
 import { Link } from "react-router-dom"
+import styled from "styled-components"
 import "./MovieCard.css"
+
+const MovieCardStyle = styled.article`
+    background-color: grey;
+    min-width: 200px;
+    position: relative;
+    overflow: hidden;
+    cursor: pointer;
+
+    img{
+      width: 100%;
+      transition: transform 0.3s ease;
+    }
+
+    &:hover img{
+      transform: scale()(1.1)
+    }
+`
+const InfoOverlay = styled.div`
+position: absolute;
+color: white;
+padding: 10px;
+`
 
 export const MovieCard = ({ id, title, release_date, poster_path }) => {
   return (
     <Link to={`/movie/${id}`}>
-      <article className="movie-card">
-        <img src={`https://image.tmdb.org/t/p/w300/${poster_path}`} alt={title}
-        />
-        <div>
+      <MovieCardStyle>
+        <InfoOverlay>
+          <img src={`https://image.tmdb.org/t/p/w300/${poster_path}`} alt={title}
+          />
           <p>Released {release_date}</p>
           <h2>{title}</h2>
-        </div>
-      </article>
+        </InfoOverlay>
+      </MovieCardStyle>
     </Link>
   )
 }
