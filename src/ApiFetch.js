014b2e267e2ca00d0917e7ca31a258b5e5b35fcd
gabster94 <iofe.gabriella@gmail.com>
2024-11-14 21:57:23 +0100

    .env for api-key and fetch function in apifetch.js

---
+++ src/ApiFetch.js
@@ -0,0 +1,26 @@
+//These functions fetches data from api
+
+//API key
+const apiEnv = import.meta.env.VITE_API_KEY;
+
+// Fetches all popular movies
+export const fetchMovies = async () => {
+  const response = await fetch(
+    `https://api.themoviedb.org/3/movie/popular?api_key=${apiEnv}&language=en-US&page=1`
+  );
+  if (!response.ok) {
+    throw new Error('Failed to fetch movies');
+  }
+  return await response.json();
+};
+
+//Fetches movie details which will be displayed in The MovieProfile
+export const fetchMovieDetails = async (id) => {
+  const response = await fetch(
+    `https://api.themoviedb.org/3/movie/${id}?api_key=${apiEnv}&language=en-US`
+  );
+  if (!response.ok) {
+    throw new Error('Failed to fetch movie details');
+  }
+  return await response.json();
+};
