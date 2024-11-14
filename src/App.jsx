11b53264d2023435861a2a720fa7d9f8ad3c6c91
Erika Olsson <erikamolsson87@gmail.com>
2024-11-14 09:58:48 +0100

    update: url

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,18 @@
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import MovieFlex from "./component/MovieFlex";
+
 export const App = () => {
-  return <div>Find me in src/app.jsx!</div>;
+  return (
+    <>
+      <BrowserRouter>   
+        <MovieFlex />
+
+        <Routes>
+          <Route path="/" element={<MovieFlex />} />
+        </Routes>
+
+      </BrowserRouter> 
+    </>
+  )
 };
---
+++ src/component/MovieFlex.jsx
@@ -0,0 +1,80 @@
+import React, { useEffect, useState } from "react";
+import PopularMoviesData from "./MovieData";
+import styled from "styled-components";
+import { H2, P} from "../ui/Typography";
+
+
+
+const FlexMovies = styled.section `
+    display: flex;
+    flex-wrap: wrap;
+`;
+
+const FlexA = styled.a `
+    width: 100%;
+    position: relative;
+    color: #fff;
+    text-decoration: none;
+    
+
+    @media (min-width: 768px) {
+        width: 33%;
+    }
+`;
+
+const FlexImg = styled.img `
+    width: 100%;
+    height: auto;
+`;
+
+const FlexImgDetails = styled.article `
+    position: absolute;
+    top: 0;
+    right: 0;
+    bottom: 0;
+    left: 0;
+    padding: 20px;
+    background-color: #000000bf;
+    opacity: 0; /* Hide the text/background */
+    transition: opacity 0.3s ease;
+
+    &:hover {
+        opacity: 1; /* Show the icons when hovering over the image */
+        cursor: pointer;
+    }
+`;
+
+
+
+const MovieFlex = ({ moviesData }) => {
+    const [movies, setMovies] = useState([]);
+
+    useEffect(() => {
+        const getMovies = async () => {
+        const data = await PopularMoviesData();
+        if (data && data.results) {
+            setMovies(data.results);
+        }
+        };
+        getMovies();
+    }, []);
+
+    return (
+        <FlexMovies>
+        {movies.map((movie) =>
+            <FlexA key={movie.id} href={url}>
+                <FlexImg src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} alt={movie.title} />
+                <FlexImgDetails>
+                    <H2>{movie.title}</H2>
+                    <P>{movie.release_date}</P>
+                </FlexImgDetails>
+            </FlexA>
+        )};
+        </FlexMovies>
+      );
+};
+/* {album.images?.[0]?.url} */
+
+
+
+export default MovieFlex;
