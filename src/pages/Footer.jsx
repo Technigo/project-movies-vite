2d82b21b109ae87d8d7726026c06f3bf82be65f1
xingyin2024 <xingyin.thu@gmail.com>
2024-11-15 21:05:57 +0100

    create - add files and TMDB API Terms of Use in footer

---
+++ src/pages/Footer.jsx
@@ -0,0 +1,11 @@
+
+
+export const Footer = () => {
+  let APITermsOfUse = "This website uses TMDB and the TMDB APIs but is not endorsed, certified, or otherwise approved by TMDB."
+
+  return (
+    <div>
+      <p>{ APITermsOfUse }</p>
+    </div>
+  );
+};
