2f5669b414c09a53078d7ee1bba27081470c7334
gabster94 <iofe.gabriella@gmail.com>
2024-11-16 18:28:20 +0100

    changed font in index.css and @media for gallery mobile-desktop

--- src/pages/MovieGallery.jsx
+++ src/pages/MovieGallery.jsx
@@ -2,8 +2,30 @@ import { useState, useEffect } from "react";
 import { MovieCard } from "../components/MovieCard";
 import styled from "styled-components";
 
-const Gallery = styled.dic `
+const Gallery = styled.div `
+  display: grid;
 
+  //mobile first
+  grid-template-columns: repeat(1, 1fr);
+
+  //big phone
+  @media (min-width: 530px) {
+    grid-template-columns: repeat(2, 1fr);
+  }
+
+  //tablet small
+  @media (min-width: 768px) {
+    grid-template-columns: repeat(3, 1fr);
+  }
+
+  @media (min-width: 1024px) {
+    grid-template-columns: repeat(4, 1fr);
+  }
+
+    /* Prevent horizontal scrolling */
+    max-width: 100%;
+  overflow-x: hidden;
+  box-sizing: border-box;
 `;
 
 // Fetches all popular movies
@@ -35,7 +57,6 @@ export const MovieGallery = () => {
 
   return (
     <Gallery>
-
       {movies.map((movie) => (
         <MovieCard 
           key={movie.id}
@@ -46,8 +67,7 @@ export const MovieGallery = () => {
           // <img 
           //   src={`https://image.tmdb.org/t/p/w300${movie.poster_path}`} 
           //   alt={movie.title} 
-          />
-        
+        />
       ))}
     </Gallery>
   )
