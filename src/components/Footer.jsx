63f677e6ab7be641342b1ee02bacb036ef1a1465
xingyin2024 <xingyin.thu@gmail.com>
2024-11-16 19:09:06 +0100

    update style of header and footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,12 +1,19 @@
+import "./Footer.css";
 import { SiThemoviedatabase } from "react-icons/si";
 
 export const Footer = () => {
   let APITermsOfUse = "This website uses TMDB and the TMDB APIs but is not endorsed, certified, or otherwise approved by TMDB."
 
   return (
-    <div>
-      <SiThemoviedatabase className="MDTB-icon"/>
-      <p>{APITermsOfUse}</p>
-    </div>
+    <footer>
+      <div className="footer-text-box">
+        <SiThemoviedatabase className="MDTB-icon" />
+
+        <div className="footer-text-content">
+          <p>{APITermsOfUse}</p>
+          <p>&copy; 2024 XingS Movie Collection APP. All rights reserved.</p>
+        </div>
+      </div>
+    </footer>
   );
 };
